7be11360ee6eba0986166fe58ad221d0f8b80e73
brinxmat <rurik.greenall@gmail.com>
2020-07-06 08:50:33 +0200

    Parameterize ArrayList

--- list-publications/src/main/java/no/unit/nva/publication/query/PublishedPublicationsResponse.java
+++ list-publications/src/main/java/no/unit/nva/publication/query/PublishedPublicationsResponse.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import no.unit.nva.publication.model.PublicationSummary;
 
-public class PublishedPublicationsResponse  extends ArrayList  {
+public class PublishedPublicationsResponse  extends ArrayList<PublicationSummary>  {
     public PublishedPublicationsResponse(List<PublicationSummary> publicationsResponse) {
         super(publicationsResponse);
     }
