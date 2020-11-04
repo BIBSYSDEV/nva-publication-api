c631b6903b7b5ce221c5a885f38ac26922fb74a4
Roy Sindre Norangshol <rono@unit.no>
2020-11-04 15:17:20 +0100

    Remove unused code

--- publication-event-emitter/src/test/java/no/unit/nva/doi/lambda/DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest.java
+++ publication-event-emitter/src/test/java/no/unit/nva/doi/lambda/DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest.java
@@ -1,25 +1,18 @@
 package no.unit.nva.doi.lambda;
 
-import static nva.commons.handlers.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import no.unit.nva.doi.publisher.EventPublisher;
-import nva.commons.utils.Environment;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest {
 
     public static final String DYNAMODB_STREAM_EVENT = "src/test/resources/event.json";
-    private static final String WILDCARD = "*";
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private Environment environmentMock;
 
     @Test
     public void handleRequestWritesToConsoleOnValidEvent() throws Exception {
@@ -37,12 +30,6 @@ public class DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest {
         DynamodbEvent event = objectMapper.readValue(eventFile, DynamodbEvent.class);
 
         handler.handleRequest(event, context);
-    }
-
-    @BeforeEach
-    void setUp() {
-        environmentMock = mock(Environment.class);
-        when(environmentMock.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn(WILDCARD);
     }
 
     private DynamodbEventFanoutStreamRecordsEventBridgeHandler createHandler(EventPublisher eventPublisher) {
