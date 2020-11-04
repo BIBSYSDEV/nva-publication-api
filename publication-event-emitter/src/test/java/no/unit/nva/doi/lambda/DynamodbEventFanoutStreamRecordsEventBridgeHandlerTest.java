05885b8aee403e056702105b283aa468de6d7577
Roy Sindre Norangshol <rono@unit.no>
2020-11-04 13:22:54 +0100

    Linting

--- publication-event-emitter/src/test/java/no/unit/nva/doi/lambda/DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest.java
+++ publication-event-emitter/src/test/java/no/unit/nva/doi/lambda/DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest.java
@@ -1,5 +1,8 @@
 package no.unit.nva.doi.lambda;
 
+import static nva.commons.handlers.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,23 +14,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static nva.commons.handlers.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest {
 
     public static final String DYNAMODB_STREAM_EVENT = "src/test/resources/event.json";
     private static final String WILDCARD = "*";
     private final ObjectMapper objectMapper = new ObjectMapper();
     private Environment environmentMock;
-
-    @BeforeEach
-    void setUp() {
-        environmentMock = mock(Environment.class);
-        when(environmentMock.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn(WILDCARD);
-
-    }
 
     @Test
     public void handleRequestWritesToConsoleOnValidEvent() throws Exception {
@@ -39,11 +31,21 @@ public class DynamodbEventFanoutStreamRecordsEventBridgeHandlerTest {
             }
         };
 
-        DynamodbEventFanoutStreamRecordsEventBridgeHandler handler = new DynamodbEventFanoutStreamRecordsEventBridgeHandler(eventPublisher);
+        DynamodbEventFanoutStreamRecordsEventBridgeHandler handler = createHandler(eventPublisher);
         Context context = Mockito.mock(Context.class);
         File eventFile = new File(DYNAMODB_STREAM_EVENT);
         DynamodbEvent event = objectMapper.readValue(eventFile, DynamodbEvent.class);
 
         handler.handleRequest(event, context);
+    }
+
+    @BeforeEach
+    void setUp() {
+        environmentMock = mock(Environment.class);
+        when(environmentMock.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn(WILDCARD);
+    }
+
+    private DynamodbEventFanoutStreamRecordsEventBridgeHandler createHandler(EventPublisher eventPublisher) {
+        return new DynamodbEventFanoutStreamRecordsEventBridgeHandler(eventPublisher);
     }
 }
