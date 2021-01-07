f05191816cb80a7896048e6983e5a3861b7d0ace
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-01-07 20:27:47 +0100

    NP-1979: simplest test of resource service

--- create-publication/src/main/java/no/unit/nva/publication/create/CreateResourceHandler.java
+++ create-publication/src/main/java/no/unit/nva/publication/create/CreateResourceHandler.java
@@ -2,7 +2,12 @@ package no.unit.nva.publication.create;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.time.Instant;
+import no.unit.nva.model.Organization;
+import no.unit.nva.model.PublicationStatus;
 import no.unit.nva.publication.identifiers.SortableIdentifier;
+import no.unit.nva.publication.service.impl.ResourceService;
 import no.unit.nva.publication.storage.model.Resource;
 import nva.commons.handlers.ApiGatewayHandler;
 import nva.commons.handlers.RequestInfo;
@@ -10,19 +15,37 @@ import org.slf4j.LoggerFactory;
 
 public class CreateResourceHandler extends ApiGatewayHandler<Void, Resource> {
 
+    private final ResourceService resourceService;
+
     public CreateResourceHandler() {
         super(Void.class, LoggerFactory.getLogger(CreateResourceHandler.class));
+        resourceService = new ResourceService();
     }
 
     @Override
     protected Resource processInput(Void input, RequestInfo requestInfo, Context context) {
         String feideId = requestInfo.getFeideId().orElseThrow();
-        String customerId = requestInfo.getCustomerId().orElseThrow();
+        URI customerId = getCustomerUri(requestInfo);
         logger.info("FeideId:" + feideId);
         logger.info("CustomerId:" + customerId);
         Resource resource = new Resource();
         resource.setIdentifier(SortableIdentifier.next());
+        resource.setPublisher(newOrganization(customerId));
+        resource.setOwner(feideId);
+        resource.setStatus(PublicationStatus.DRAFT);
+        resource.setCreatedDate(Instant.now());
+        resourceService.createResource(resource);
         return resource;
+    }
+
+    private Organization newOrganization(URI customerId) {
+        return new Organization.Builder().withId(customerId).build();
+    }
+
+    private URI getCustomerUri(RequestInfo requestInfo) {
+        return requestInfo.getCustomerId()
+            .map(URI::create)
+            .orElseThrow();
     }
 
     @Override
