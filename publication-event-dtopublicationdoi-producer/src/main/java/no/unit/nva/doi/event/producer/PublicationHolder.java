36d197c1f744494760c88139ea0656d0ca95a0d0
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-02-03 09:10:56 +0100

    NP-1979: DynamoDbFanoutPublicationDtoProducer uses datamodel-java

---
+++ publication-event-dtopublicationdoi-producer/src/main/java/no/unit/nva/doi/event/producer/PublicationHolder.java
@@ -0,0 +1,47 @@
+package no.unit.nva.doi.event.producer;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+import no.unit.nva.model.Publication;
+import nva.commons.core.JacocoGenerated;
+
+public class PublicationHolder {
+
+    private final String type;
+    private final Publication item;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getType(), getItem());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof PublicationHolder)) {
+            return false;
+        }
+        PublicationHolder that = (PublicationHolder) o;
+        return Objects.equals(getType(), that.getType()) && Objects.equals(getItem(), that.getItem());
+    }
+
+    @JacocoGenerated
+    @JsonCreator
+    public PublicationHolder(
+        @JsonProperty("type") String type,
+        @JsonProperty("item") Publication publication) {
+        this.type = type;
+        this.item = publication;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Publication getItem() {
+        return item;
+    }
+}
