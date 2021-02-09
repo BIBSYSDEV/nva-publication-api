348ff45b938cb382d7b1e1e4e198b308bf054259
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-02-09 13:00:43 +0100

    NP-1979: doi-event-producer-decides-on-event

--- publication-event-dtopublicationdoi-producer/src/main/java/no/unit/nva/doi/event/producer/DynamoDbFanoutPublicationDtoProducer.java
+++ publication-event-dtopublicationdoi-producer/src/main/java/no/unit/nva/doi/event/producer/DynamoDbFanoutPublicationDtoProducer.java
@@ -2,6 +2,8 @@ package no.unit.nva.doi.event.producer;
 
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
+import static nva.commons.core.JsonUtils.objectMapper;
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.util.Optional;
 import no.unit.nva.events.handlers.DestinationsEventBridgeEventHandler;
@@ -21,11 +23,14 @@ import org.slf4j.LoggerFactory;
 public class DynamoDbFanoutPublicationDtoProducer
     extends DestinationsEventBridgeEventHandler<DynamoEntryUpdateEvent, PublicationHolder> {
 
-    public static final String TYPE_DTO_DOI_PUBLICATION = "publication.doiupdate.request";
+    public static final String TYPE_REQUEST_FOR_NEW_DRAFT_DOI = "publication.doiupdate.newdraftdoirequest";
+    private static final String TYPE_UPDATE_DRAFT_DOI = "publication.doiupdate.updatedraftdoi";
+    public static final String TYPE_RESOURCE_WITH_FINDABLE_DOI_UPDATE = "publication.doiupdate.updatefindabledoi";
     public static final String NO_RESOURCE_IDENTIFIER_ERROR = "Resource has no identifier:";
     private static final String EMPTY_EVENT_TYPE = "empty";
     public static final PublicationHolder EMPTY_EVENT = emptyEvent();
     private static final Logger logger = LoggerFactory.getLogger(DynamoDbFanoutPublicationDtoProducer.class);
+    public static final String ILLEGA_STATE_WHILE_UPDATING_DOI = "Illegal state while updating doi: ";
 
     @JacocoGenerated
     public DynamoDbFanoutPublicationDtoProducer() {
@@ -63,8 +68,27 @@ public class DynamoDbFanoutPublicationDtoProducer
         return Optional.of(updateEvent)
             .filter(this::shouldPropagateEvent)
             .map(DynamoEntryUpdateEvent::getNewPublication)
-            .map(pub -> new PublicationHolder(TYPE_DTO_DOI_PUBLICATION, pub))
+            .map(pub -> new PublicationHolder(calculateEventType(updateEvent), pub))
             .orElse(EMPTY_EVENT);
+    }
+
+    private String calculateEventType(DynamoEntryUpdateEvent updateEvent) {
+        if(isNull(updateEvent.getOldPublication())){
+            return TYPE_REQUEST_FOR_NEW_DRAFT_DOI;
+        }
+        if(nonNull(updateEvent.getOldPublication()) && isNull(updateEvent.getNewPublication().getDoi())){
+            return TYPE_UPDATE_DRAFT_DOI;
+        }
+        if(nonNull(updateEvent.getOldPublication())&& nonNull(updateEvent.getNewPublication().getDoi())){
+            return TYPE_RESOURCE_WITH_FINDABLE_DOI_UPDATE;
+        }
+
+        throw handleIllegalState(updateEvent);
+    }
+
+    private IllegalStateException handleIllegalState(DynamoEntryUpdateEvent updateEvent) {
+        String json = attempt(() -> objectMapper.writeValueAsString(updateEvent)).orElseThrow();
+        return new IllegalStateException(ILLEGA_STATE_WHILE_UPDATING_DOI + json);
     }
 
     private boolean publicationHasDoiRequest(DynamoEntryUpdateEvent updateEvent) {
