436c803a61c6f675185ba36642360274b6d82a19
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-02-22 21:02:43 +0100

    NP-1812: fix logging in CreateMessageHandler

--- create-message/src/main/java/no/unit/nva/publication/messages/create/CreateMessageHandler.java
+++ create-message/src/main/java/no/unit/nva/publication/messages/create/CreateMessageHandler.java
@@ -1,5 +1,6 @@
 package no.unit.nva.publication.messages.create;
 
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -22,6 +23,7 @@ import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,6 +53,8 @@ public class CreateMessageHandler extends ApiGatewayHandler<CreateMessageRequest
     @Override
     protected Void processInput(CreateMessageRequest input, RequestInfo requestInfo, Context context)
         throws ApiGatewayException {
+        String json = attempt(() -> JsonUtils.objectMapper.writeValueAsString(input)).orElseThrow();
+        logger.info(json);
         UserInstance sender = createSender(requestInfo);
 
         Publication publication = fetchExistingPublication(input);
