2b0009826d6600f5797da835cc49daa22a1bf744
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-03-06 13:20:37 +0100

    linting

--- publication-commons/src/main/java/no/unit/nva/publication/ServiceEnvironmentConstants.java
+++ publication-commons/src/main/java/no/unit/nva/publication/ServiceEnvironmentConstants.java
@@ -4,15 +4,13 @@ import static no.unit.nva.publication.storage.model.DatabaseConstants.environmen
 
 public final class ServiceEnvironmentConstants {
 
-    private static final String HOST_ENV_VARIABLE_NAME = "API_HOST";
-    public static final String API_HOST = environment.readEnv(HOST_ENV_VARIABLE_NAME);
-    private static final String NETWORK_SCHEME_ENV_VARIABLE_NAME = "API_SCHEME";
-    ;
-    public static final String API_SCHEME = environment.readEnv(NETWORK_SCHEME_ENV_VARIABLE_NAME);
-    ;
     public static final String URI_EMPTY_FRAGMENT = null;
     public static final String PATH_SEPARATOR = "/";
     public static final String MESSAGE_PATH = "/messages";
+    private static final String HOST_ENV_VARIABLE_NAME = "API_HOST";
+    public static final String API_HOST = environment.readEnv(HOST_ENV_VARIABLE_NAME);
+    private static final String NETWORK_SCHEME_ENV_VARIABLE_NAME = "API_SCHEME";
+    public static final String API_SCHEME = environment.readEnv(NETWORK_SCHEME_ENV_VARIABLE_NAME);
 
     private ServiceEnvironmentConstants() {
 
