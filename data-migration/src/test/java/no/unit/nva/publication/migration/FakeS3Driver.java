e0b35ad6619f31681e793e2d538ed23119faa17c
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-04-14 13:21:17 +0200

    data-migration uses IonReader

--- data-migration/src/test/java/no/unit/nva/publication/migration/FakeS3Driver.java
+++ data-migration/src/test/java/no/unit/nva/publication/migration/FakeS3Driver.java
@@ -4,7 +4,9 @@ import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.HOURS;
 import static java.time.temporal.ChronoUnit.MILLIS;
 import static nva.commons.core.JsonUtils.objectMapper;
+import static nva.commons.core.JsonUtils.objectMapperNoEmpty;
 import static nva.commons.core.attempt.Try.attempt;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.javafaker.Faker;
@@ -13,16 +15,21 @@ import java.net.URI;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import no.unit.nva.dataimport.S3IonReader;
 import no.unit.nva.model.DoiRequest;
 import no.unit.nva.model.DoiRequestMessage;
 import no.unit.nva.model.DoiRequestStatus;
 import no.unit.nva.model.Publication;
 import no.unit.nva.publication.PublicationGenerator;
 import no.unit.nva.s3.S3Driver;
-import nva.commons.core.attempt.Try;
 import software.amazon.ion.system.IonReaderBuilder;
 import software.amazon.ion.system.IonTextWriterBuilder;
 
@@ -34,7 +41,9 @@ class FakeS3Driver extends S3Driver {
     public static final List<Publication> PUBLICATIONS_WITHOUT_DOI_REQUESTS = samplePublications(WITHOUT_DOI_REQUEST);
     public static final URI PUBLISHER_URI = PUBLICATIONS_WITHOUT_DOI_REQUESTS.get(0).getPublisher().getId();
     public static final List<Publication> PUBLICATIONS_WITH_DOI_REQUESTS = samplePublications(WITH_DOI_REQUEST);
-
+    public static final List<String> FILENAMES = sampleFilenames();
+    public static final Map<String, Set<String>> fileContent = distributeContentInFiles();
+    public static final String LINE_SEPARATOR = System.lineSeparator();
     private static final long ONE_YEAR = 365L * 24 * 60 * 60 * 1000;
 
     public FakeS3Driver() {
@@ -68,21 +77,44 @@ class FakeS3Driver extends S3Driver {
 
     @Override
     public List<String> getFiles(Path path) {
-        return List.of(PUBLICATIONS_WITHOUT_DOI_REQUESTS, PUBLICATIONS_WITH_DOI_REQUESTS)
-                   .stream()
-                   .flatMap(publications -> serializePublicationsBatch(publications).stream())
-                   .map(this::convertJsonToIon)
-                   .collect(Collectors.toList());
+        throw new UnsupportedOperationException();
     }
 
-    public List<String> serializePublicationsBatch(List<Publication> firstPublicationsFile) {
+    @Override
+    public List<String> listFiles(Path path) {
+        return FILENAMES;
+    }
 
-        return firstPublicationsFile.stream()
-                   .map(attempt(publication -> objectMapper.convertValue(publication, JsonNode.class)))
-                   .map(attempt -> attempt.map(this::addParentItem))
-                   .map(attempt -> attempt.map(objectMapper::writeValueAsString))
-                   .map(Try::orElseThrow)
-                   .collect(Collectors.toList());
+    @Override
+    public String getFile(String filename) {
+        return String.join(LINE_SEPARATOR, fileContent.get(filename));
+    }
+
+    private static Map<String, Set<String>> distributeContentInFiles() {
+        Random random = new Random(System.currentTimeMillis());
+        List<Publication> publications = allSamplePublications();
+        Map<String, Set<String>> fileContents = new ConcurrentHashMap<>();
+        for (Publication publication : publications) {
+            String file = FILENAMES.get(random.nextInt(FILENAMES.size()));
+            String content = attempt(() -> serializePublication(publication)).orElseThrow();
+            addContentToFile(file, content, fileContents);
+        }
+        return fileContents;
+    }
+
+    private static void addContentToFile(String file, String content, Map<String, Set<String>> fileContents) {
+        if (!fileContents.containsKey(file)) {
+            fileContents.put(file, new HashSet<>());
+        }
+        fileContents.get(file).add(content);
+    }
+
+    private static List<String> sampleFilenames() {
+        return List.of(randomFilename(), randomFilename());
+    }
+
+    private static String randomFilename() {
+        return FAKER.lorem().word();
     }
 
     private static DoiRequest sampleDoiRequest() {
@@ -125,13 +157,20 @@ class FakeS3Driver extends S3Driver {
         return publicationWithDuplicate.getModifiedDate().plus(10, DAYS);
     }
 
-    private JsonNode addParentItem(JsonNode json) {
+    private static String serializePublication(Publication publication) throws JsonProcessingException {
+        JsonNode jsonNode = objectMapperNoEmpty.convertValue(publication, JsonNode.class);
+        JsonNode withIonParent = addParentItem(jsonNode);
+        String jsonString = objectMapperNoEmpty.writeValueAsString(withIonParent);
+        return convertJsonToIon(jsonString);
+    }
+
+    private static JsonNode addParentItem(JsonNode json) {
         ObjectNode root = objectMapper.createObjectNode();
-        root.set(PublicationImporter.ION_ITEM, json);
+        root.set(S3IonReader.ION_ITEM, json);
         return root;
     }
 
-    private String convertJsonToIon(String json) {
+    private static String convertJsonToIon(String json) {
         var ionReader = IonReaderBuilder.standard().build(json);
         ionReader = IonReaderBuilder.standard().build(json);
         var stringWriter = new StringBuilder();
