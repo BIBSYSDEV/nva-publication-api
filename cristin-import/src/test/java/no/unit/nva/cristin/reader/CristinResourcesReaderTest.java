e471d2b7bd9e2e1e8f93cb078ed05925eb414a64
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-04-23 12:06:09 +0200

    add remote test for reading cristin resources

--- cristin-import/src/test/java/no/unit/nva/cristin/reader/CristinResourcesReaderTest.java
+++ cristin-import/src/test/java/no/unit/nva/cristin/reader/CristinResourcesReaderTest.java
@@ -1,13 +1,17 @@
 package no.unit.nva.cristin.reader;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 import no.unit.nva.cristin.mapper.CristinObject;
 import no.unit.nva.publication.StubS3Driver;
 import no.unit.nva.s3.S3Driver;
+import nva.commons.core.Environment;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class CristinResourcesReaderTest {
@@ -15,9 +19,18 @@ public class CristinResourcesReaderTest {
     public static final String SOME_BUCKET_NAME = "someBucketName";
     public static final Path SOME_FOLDER = Path.of("some/folder");
     private static final String RESOURCE_01 = "input01.gz";
-    public static final List<String> RESOURCES_LIST = List.of(RESOURCE_01);
     private static final String RESOURCE_01_AS_JSON_ARRAY = "input01AsArray.json.gz";
     private static final int NUMBER_OF_RECORDS_IN_RESOURCES = 100;
+
+    @Test
+    @Tag("RemoteTest")
+    public void readCristinResourcesReturnsStreamOfCristinObjectsRemoteTest() {
+        String bucket = new Environment().readEnv("AWS_BUCKET");
+        S3Driver s3Driver = new S3Driver(bucket);
+        CristinReader reader = new CristinReader(s3Driver);
+        List<CristinObject> cristinObjects = reader.readResources(Path.of("")).collect(Collectors.toList());
+        assertThat(cristinObjects.size(), is(greaterThan(0)));
+    }
 
     @Test
     public void readCristinResourcesReturnsStreamOfCristinObjects() {
