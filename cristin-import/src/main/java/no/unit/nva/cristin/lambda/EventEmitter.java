9dfc37f0f36ec5d9eb180bbd7a691476e18338e5
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-04-28 10:01:39 +0200

    NP-2520: javadoc

--- cristin-import/src/main/java/no/unit/nva/cristin/lambda/EventEmitter.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/EventEmitter.java
@@ -1,18 +1,40 @@
 package no.unit.nva.cristin.lambda;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import nva.commons.core.JacocoGenerated;
 import nva.commons.core.JsonSerializable;
+import org.apache.commons.collections4.ListUtils;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsResponse;
 
+/**
+ * Emits events in AWS EventBridge each event has the following format:
+ * <p><pre>{@code
+ * {
+ *   "version": "0",
+ *   "id": "6a7e8feb-b491-4cf7-a9f1-bf3703467718",
+ *   "detail-type": "EC2 Instance State-change Notification",
+ *   "source": "aws.ec2",
+ *   "account": "111122223333",
+ *   "time": "2017-12-22T18:43:48Z",
+ *   "region": "us-west-1",
+ *   "resources": [
+ *     "arn:aws:ec2:us-west-1:123456789012:instance/i-1234567890abcdef0"
+ *   ],
+ *   "detail": {
+ *     "someField": "someValue",
+ *     "someOtherField": "someOtherValue"
+ *   }
+ * }
+ * }</pre>
+ *
+ * @param <T> the type of the event detail.
+ */
 public class EventEmitter<T extends JsonSerializable> {
 
     private static final int BATCH_SIZE = 10;
@@ -28,14 +50,22 @@ public class EventEmitter<T extends JsonSerializable> {
         this.client = eventBridgeClient;
     }
 
-    public void addEvents(Collection<T> events) {
-
-        List<PutEventsRequestEntry> entries = events.stream()
-                                                  .map(this::createPutEventRequesEntry)
-                                                  .collect(Collectors.toList());
-        putEventsRequests = createBatchesOfPutEventsRequests(entries);
+    /**
+     * Create one event for every element in the "eventDetails" list.
+     *
+     * @param eventDetails A collection of details for the respective events.
+     */
+    public void addEvents(Collection<T> eventDetails) {
+        List<PutEventsRequestEntry> eventRequestEntries =
+            eventDetails.stream().map(this::createPutEventRequestEntry).collect(Collectors.toList());
+        putEventsRequests = createBatchesOfPutEventsRequests(eventRequestEntries);
     }
 
+    /**
+     * This method emits the events in AWS EventBridge.
+     *
+     * @return The failed requests and the respective responses from EventBridge.
+     */
     public List<PutEventsResult> emitEvents() {
         List<PutEventsResult> failedEvents = emitEventsAndCollectFailures(putEventsRequests);
         int attempts = 0;
@@ -51,35 +81,25 @@ public class EventEmitter<T extends JsonSerializable> {
         return Collections.emptyList();
     }
 
-    private PutEventsRequestEntry createPutEventRequesEntry(T filenameEvent) {
+    private PutEventsRequestEntry createPutEventRequestEntry(T eventDetail) {
 
         return PutEventsRequestEntry.builder()
                    .eventBusName(ApplicationConstants.EVENT_BUS_NAME)
                    .resources(invokingFunctionArn)
                    .detailType(detailType)
                    .time(Instant.now())
-                   .detail(filenameEvent.toJsonString())
+                   .detail(eventDetail.toJsonString())
                    .source(invokingFunctionArn)
                    .build();
     }
 
     private List<PutEventsRequest> createBatchesOfPutEventsRequests(List<PutEventsRequestEntry> entries) {
-        List<PutEventsRequest> eventsRequests = new ArrayList<>();
-
-        for (int subListStartIndex = 0; subListStartIndex < entries.size(); subListStartIndex += BATCH_SIZE) {
-            int subListEndIndex = endIndex(subListStartIndex, entries);
-            List<PutEventsRequestEntry> sublist = entries.subList(subListStartIndex, subListEndIndex);
-            PutEventsRequest putEventsRequest = PutEventsRequest.builder().entries(sublist).build();
-            eventsRequests.add(putEventsRequest);
-        }
-        return eventsRequests;
+        return ListUtils.partition(entries, BATCH_SIZE)
+                   .stream()
+                   .map(batch -> PutEventsRequest.builder().entries(batch).build())
+                   .collect(Collectors.toList());
     }
 
-    private int endIndex(int index, List<PutEventsRequestEntry> originalList) {
-        return Math.min(index + BATCH_SIZE, originalList.size());
-    }
-
-    @JacocoGenerated
     private List<PutEventsRequest> collectRequestsForResending(List<PutEventsResult> failedEvents) {
         return failedEvents.stream().map(PutEventsResult::getRequest).collect(Collectors.toList());
     }
