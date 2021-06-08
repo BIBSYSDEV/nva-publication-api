7ff32593a70f23593701c40cddc9ef667f445c11
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-06-08 16:37:04 +0200

    NP-2674-use-explicit-utf8-character-for-encoding

--- cristin-import/src/main/java/no/unit/nva/cristin/mapper/CristinContributorRoleCode.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/mapper/CristinContributorRoleCode.java
@@ -2,26 +2,15 @@ package no.unit.nva.cristin.mapper;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 public enum CristinContributorRoleCode {
     CREATOR("FORFATTER"),
-    EDITOR("REDAKTØR");
+    EDITOR("REDAKT" + "\u00D8" + "R");
 
     private final String value;
 
-    public static final String EDITOR_NOR = new String("REDAKTØR".getBytes(StandardCharsets.UTF_8),
-        StandardCharsets.UTF_8);
-    public static final String CREATOR_NOR = new String("FORFATTER".getBytes(StandardCharsets.UTF_8),
-        StandardCharsets.UTF_8);
     public static final String UNKNOWN_ROLE_ERROR = "Unmapped alias for roleCode: ";
-    private static final Map<String, CristinContributorRoleCode> ALIASES_MAP = constructAliasesMap();
-    private static final Map<CristinContributorRoleCode, String> DEFAULT_NAMES = constructDefaultNames();
 
     CristinContributorRoleCode(String value) {
         this.value = value;
@@ -30,26 +19,13 @@ public enum CristinContributorRoleCode {
     @JsonCreator
     public static CristinContributorRoleCode fromString(String roleCode) {
         return Arrays.stream(CristinContributorRoleCode.values())
-            .filter(value -> value.value.equalsIgnoreCase(roleCode))
-            .findAny()
-            .orElseThrow(() -> new RuntimeException(UNKNOWN_ROLE_ERROR + roleCode));
+                   .filter(role -> role.getStringValue().equalsIgnoreCase(roleCode))
+                   .findAny()
+                   .orElseThrow(() -> new RuntimeException(UNKNOWN_ROLE_ERROR + roleCode));
     }
 
     @JsonValue
     public String getStringValue() {
-        return DEFAULT_NAMES.get(this);
-    }
-
-    private static Map<CristinContributorRoleCode, String> constructDefaultNames() {
-        return Map.of(
-            EDITOR, EDITOR_NOR,
-            CREATOR, CREATOR_NOR);
-    }
-
-    private static Map<String, CristinContributorRoleCode> constructAliasesMap() {
-        Map<String, CristinContributorRoleCode> aliasesMap = new ConcurrentHashMap<>();
-        aliasesMap.put(EDITOR_NOR, EDITOR);
-        aliasesMap.put(CREATOR_NOR, CREATOR);
-        return Collections.unmodifiableMap(aliasesMap);
+        return value;
     }
 }
