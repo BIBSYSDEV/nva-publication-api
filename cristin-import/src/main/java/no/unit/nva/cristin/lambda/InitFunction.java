55667005bc95dd6ba6295a4591781841a70f6e12
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-05 22:28:58 +0200

    init function

---
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/InitFunction.java
@@ -0,0 +1,31 @@
+package no.unit.nva.cristin.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import nva.commons.core.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InitFunction implements RequestStreamHandler {
+
+    private final static Logger logger= LoggerFactory.getLogger(InitFunction.class);
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        SimpleMessage simpleMessage = new SimpleMessage("hello");
+        String messageString= JsonUtils.objectMapperWithEmpty.writeValueAsString(simpleMessage);
+        logger.info(messageString);
+        writeOutput(messageString,output);
+    }
+
+    private void writeOutput(String messageString, OutputStream outputStream) throws IOException {
+        try(BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(outputStream))){
+          writer.write(messageString);
+        }
+    }
+}
---
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/SimpleMessage.java
@@ -0,0 +1,14 @@
+package no.unit.nva.cristin.lambda;
+
+public class SimpleMessage {
+
+    private final String message;
+
+    public SimpleMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
