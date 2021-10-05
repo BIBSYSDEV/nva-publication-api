7c8f3b03b2da3cc7c9778d0b750381054d30e5b1
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-05 22:46:07 +0200

    jacoco ignores all about init function

--- cristin-import/src/main/java/no/unit/nva/cristin/lambda/InitFunction.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/InitFunction.java
@@ -7,14 +7,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import nva.commons.core.JacocoGenerated;
 import nva.commons.core.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+@JacocoGenerated
 public class InitFunction implements RequestStreamHandler {
 
     private final static Logger logger= LoggerFactory.getLogger(InitFunction.class);
 
+    @JacocoGenerated
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
         SimpleMessage simpleMessage = new SimpleMessage("hello");
@@ -23,6 +27,7 @@ public class InitFunction implements RequestStreamHandler {
         writeOutput(messageString,output);
     }
 
+    @JacocoGenerated
     private void writeOutput(String messageString, OutputStream outputStream) throws IOException {
         try(BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(outputStream))){
           writer.write(messageString);
--- cristin-import/src/main/java/no/unit/nva/cristin/lambda/SimpleMessage.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/SimpleMessage.java
@@ -1,13 +1,17 @@
 package no.unit.nva.cristin.lambda;
 
+import nva.commons.core.JacocoGenerated;
+
 public class SimpleMessage {
 
     private final String message;
 
+    @JacocoGenerated
     public SimpleMessage(String message) {
         this.message = message;
     }
 
+    @JacocoGenerated
     public String getMessage() {
         return message;
     }
