8022e40a4deb417313fe88925442e5df0615edc1
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-28 16:21:03 +0200

    override JsonSerializables default method

--- expansion/src/main/java/no/unit/nva/expansion/model/ExpandedResourceUpdate.java
+++ expansion/src/main/java/no/unit/nva/expansion/model/ExpandedResourceUpdate.java
@@ -1,7 +1,16 @@
 package no.unit.nva.expansion.model;
 
 import nva.commons.core.JsonSerializable;
+import nva.commons.core.JsonUtils;
 
 public interface ExpandedResourceUpdate extends JsonSerializable {
+
+    default String toJsonString() {
+        try {
+            return JsonUtils.dtoObjectMapper.writeValueAsString(this);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
