4feda5e87cbe728f0cbde269ec636512b6fab200
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-12-10 13:38:48 +0100

    update CreatePublicationHandlerTest with current expectations

--- publication-rest/src/test/java/no/unit/nva/publication/create/CreatePublicationHandlerTest.java
+++ publication-rest/src/test/java/no/unit/nva/publication/create/CreatePublicationHandlerTest.java
@@ -1,54 +1,54 @@
 package no.unit.nva.publication.create;
 
-import static no.unit.nva.publication.PublicationRestHandlersTestConfig.restApiMapper;
+import static no.unit.nva.publication.PublicationServiceConfig.dtoObjectMapper;
 import static no.unit.nva.publication.create.CreatePublicationHandler.API_HOST;
 import static no.unit.nva.publication.create.CreatePublicationHandler.API_SCHEME;
-import static no.unit.nva.publication.testing.TestHeaders.getRequestHeaders;
 import static no.unit.nva.publication.testing.TestHeaders.getResponseHeaders;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static nva.commons.apigateway.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.net.HttpHeaders;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URI;
-import java.time.Instant;
+import java.time.Clock;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-import no.unit.nva.PublicationMapper;
 import no.unit.nva.api.CreatePublicationRequest;
 import no.unit.nva.api.PublicationResponse;
+import no.unit.nva.file.model.FileSet;
 import no.unit.nva.identifiers.SortableIdentifier;
-import no.unit.nva.model.Organization;
 import no.unit.nva.model.Publication;
-import no.unit.nva.publication.RequestUtil;
+import no.unit.nva.model.testing.PublicationGenerator;
+import no.unit.nva.publication.service.ResourcesLocalTest;
 import no.unit.nva.publication.service.impl.ResourceService;
+import no.unit.nva.publication.testing.http.FakeHttpClient;
+import no.unit.nva.testutils.HandlerRequestBuilder;
 import nva.commons.apigateway.GatewayResponse;
 import nva.commons.core.Environment;
-import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CreatePublicationHandlerTest {
+public class CreatePublicationHandlerTest extends ResourcesLocalTest {
 
     public static final String HTTPS = "https";
     public static final String NVA_UNIT_NO = "nva.unit.no";
     public static final String WILDCARD = "*";
-    public static final String AUTHORIZER = "authorizer";
-    public static final String CLAIMS = "claims";
-    public static final String TEST_FEIDE_ID = "junit";
-    public static final String TEST_ORG_NUMBER = "NO919477822";
-    public static final String REQUEST_CONTEXT = "requestContext";
-    public static final String HEADERS = "headers";
-    public static final String BODY = "body";
-    private ResourceService publicationServiceMock;
+    public static final String TEST_FEIDE_ID = randomString();
+    public static final URI TEST_ORG_ID = randomUri();
+
+    public static final Clock CLOCK = Clock.systemDefaultZone();
     private CreatePublicationHandler handler;
     private ByteArrayOutputStream outputStream;
     private Context context;
@@ -58,49 +58,56 @@ public class CreatePublicationHandlerTest {
      */
     @BeforeEach
     public void setUp() {
-        publicationServiceMock = mock(ResourceService.class);
+        super.init();
         Environment environmentMock = mock(Environment.class);
         when(environmentMock.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn(WILDCARD);
         when(environmentMock.readEnv(API_SCHEME)).thenReturn(HTTPS);
         when(environmentMock.readEnv(API_HOST)).thenReturn(NVA_UNIT_NO);
-        handler = new CreatePublicationHandler(publicationServiceMock, environmentMock);
+        ResourceService resourceService = new ResourceService(client, new FakeHttpClient(), CLOCK);
+        handler = new CreatePublicationHandler(resourceService, environmentMock);
         outputStream = new ByteArrayOutputStream();
         context = mock(Context.class);
     }
 
     @Test
-    public void requestToHandlerReturnsCustomerCreated() throws Exception {
-        Publication publication = createPublication();
-        when(publicationServiceMock.createPublication(any(Publication.class))).thenReturn(publication);
+    public void requestToHandlerReturnsMinRequiredFieldsWhenRequestContainsEmptyResource() throws Exception {
 
-        CreatePublicationRequest request = new CreatePublicationRequest();
-        request.setEntityDescription(publication.getEntityDescription());
-        request.setProjects(publication.getProjects());
+        CreatePublicationRequest request = createEmptyPublicationRequest();
         InputStream inputStream = createPublicationRequest(request);
         handler.handleRequest(inputStream, outputStream, context);
 
         GatewayResponse<PublicationResponse> actual = GatewayResponse.fromOutputStream(outputStream);
+        assertThat(actual.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_CREATED)));
+        var publicationResponse = actual.getBodyObject(PublicationResponse.class);
+        assertExistenceOfMinimumRequiredFields(publicationResponse);
+    }
 
-        GatewayResponse<PublicationResponse> expected = new GatewayResponse<>(
-            PublicationMapper.convertValue(publication, PublicationResponse.class),
-            getResponseHeadersWithLocation(publication.getIdentifier()),
-            HttpStatus.SC_CREATED,
-            restApiMapper
-        );
-
-        assertEquals(expected, actual);
+    private CreatePublicationRequest createEmptyPublicationRequest() {
+        return new CreatePublicationRequest();
     }
 
     @Test
-    public void canCreateNewPublication() throws Exception {
-        Publication publication = createPublication();
-        when(publicationServiceMock.createPublication(any(Publication.class))).thenReturn(publication);
+    public void requestToHandlerReturnsResourceWithFilSetWhenRequestContainsFileSet() throws Exception {
+        FileSet filesetInCreationRequest = PublicationGenerator.randomPublication().getFileSet();
+        CreatePublicationRequest request = createEmptyPublicationRequest();
+        request.setFileSet(filesetInCreationRequest);
 
-        InputStream inputStream = emptyCreatePublicationRequest();
+        InputStream inputStream = createPublicationRequest(request);
         handler.handleRequest(inputStream, outputStream, context);
+
         GatewayResponse<PublicationResponse> actual = GatewayResponse.fromOutputStream(outputStream);
-        assertEquals(HttpStatus.SC_CREATED, actual.getStatusCode());
-        assertNotNull(actual.getBodyObject(PublicationResponse.class));
+        assertThat(actual.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_CREATED)));
+        var publicationResponse = actual.getBodyObject(PublicationResponse.class);
+        assertThat(publicationResponse.getFileSet(),is(equalTo(filesetInCreationRequest)));
+        assertExistenceOfMinimumRequiredFields(publicationResponse);
+    }
+
+    private void assertExistenceOfMinimumRequiredFields(PublicationResponse publicationResponse) {
+        assertThat(publicationResponse.getIdentifier(), is(not(nullValue())));
+        assertThat(publicationResponse.getIdentifier(), is(instanceOf(SortableIdentifier.class)));
+        assertThat(publicationResponse.getCreatedDate(), is(not(nullValue())));
+        assertThat(publicationResponse.getOwner(), is(equalTo(TEST_FEIDE_ID)));
+        assertThat(publicationResponse.getPublisher().getId(), is(equalTo(TEST_ORG_ID)));
     }
 
     private Map<String, String> getResponseHeadersWithLocation(SortableIdentifier identifier) {
@@ -110,42 +117,10 @@ public class CreatePublicationHandlerTest {
     }
 
     private InputStream createPublicationRequest(CreatePublicationRequest request) throws JsonProcessingException {
-        Map<String, Object> map = Map.of(
-            REQUEST_CONTEXT, createRequestContext(),
-            HEADERS, getRequestHeaders(),
-            BODY, request
-        );
-        return new ByteArrayInputStream(restApiMapper.writeValueAsBytes(map));
-    }
-
-    private InputStream emptyCreatePublicationRequest() throws JsonProcessingException {
-        Map<String, Object> map = Map.of(
-            REQUEST_CONTEXT, createRequestContext(),
-            HEADERS, getRequestHeaders()
-        );
-        return new ByteArrayInputStream(restApiMapper.writeValueAsBytes(map));
-    }
-
-    private Map<String, Map<String, Map<String, String>>> createRequestContext() {
-        return Map.of(
-            AUTHORIZER, Map.of(
-                CLAIMS, Map.of(
-                    RequestUtil.CUSTOM_FEIDE_ID, TEST_FEIDE_ID,
-                    RequestUtil.CUSTOM_CUSTOMER_ID, TEST_ORG_NUMBER
-                )
-            )
-        );
-    }
-
-    private Publication createPublication() {
-        return new Publication.Builder()
-            .withIdentifier(new SortableIdentifier(UUID.randomUUID().toString()))
-            .withModifiedDate(Instant.now())
-            .withOwner("owner")
-            .withPublisher(new Organization.Builder()
-                               .withId(URI.create("http://example.org/publisher/1"))
-                               .build()
-            )
+        return new HandlerRequestBuilder<CreatePublicationRequest>(dtoObjectMapper)
+            .withFeideId(TEST_FEIDE_ID)
+            .withCustomerId(TEST_ORG_ID.toString())
+            .withBody(request)
             .build();
     }
 }
