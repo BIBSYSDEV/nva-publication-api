0763032b91bd9bfd61711df4093a2a06d56c9c0c
Jan Bjørndalen <jbj@aztek.no>
2022-02-07 09:47:52 +0100

    Added logging in AffilicationSelectionService

--- publication-commons/src/main/java/no/unit/nva/publication/service/impl/AffiliationSelectionService.java
+++ publication-commons/src/main/java/no/unit/nva/publication/service/impl/AffiliationSelectionService.java
@@ -9,10 +9,14 @@ import java.util.stream.Collectors;
 import no.unit.nva.publication.external.services.PersonApiClient;
 import no.unit.nva.publication.utils.OrgUnitId;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class AffiliationSelectionService {
 
     private final PersonApiClient personApiClient;
+
+    private static final Logger logger = LoggerFactory.getLogger(AffiliationSelectionService.class);
 
     private AffiliationSelectionService(PersonApiClient personApiClient) {
         this.personApiClient = personApiClient;
@@ -35,6 +39,7 @@ public final class AffiliationSelectionService {
 
     private List<OrgUnitId> fetchAffiliationUris(String feideId)
         throws IOException, ApiGatewayException, InterruptedException {
+        logger.debug("Fetching affiliation uris for feideid:{}", feideId);
         return personApiClient.fetchAffiliationsForUser(feideId)
             .stream()
             .map(OrgUnitId::new)
