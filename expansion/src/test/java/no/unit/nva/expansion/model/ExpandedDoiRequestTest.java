265692f66efacc0c64f97f5e9c9452d38ce0d109
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-05-13 10:58:08 +0200

    fix resource reference in expanded doi requests

---
+++ expansion/src/test/java/no/unit/nva/expansion/model/ExpandedDoiRequestTest.java
@@ -0,0 +1,14 @@
+package no.unit.nva.expansion.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.publication.storage.model.DoiRequest;
+import org.junit.jupiter.api.Test;
+
+class ExpandedDoiRequestTest {
+
+
+
+
+
+}
