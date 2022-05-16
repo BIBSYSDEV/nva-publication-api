a1cf3dfe753f5dc941bcce1486df2d16e45e32b3
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-05-16 21:29:14 +0200

    NP-9127: DynamoDbStream-to-EventBridge handler saves event in events S3 bucket

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandler.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandler.java
@@ -1,16 +1,24 @@
 package no.unit.nva.publication.events.handlers.dynamodbstream;
 
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import java.net.URI;
 import java.time.Duration;
+import java.util.UUID;
+import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.s3.S3Driver;
+import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import nva.commons.core.paths.UnixPath;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
 import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 /**
@@ -19,25 +27,35 @@ import software.amazon.awssdk.services.sqs.SqsClient;
  * <p>Notice a DynamoDB stream can only have two streams attached before we it can lead into throttling and performance
  * issues with DynamodDB, this is why we have this handler to publish it to EventBridge.
  */
-public class DynamodbStreamToEventBridgeHandler implements RequestHandler<DynamodbEvent, Void> {
+public class DynamodbStreamToEventBridgeHandler implements RequestHandler<DynamodbEvent, URI> {
 
     public static final String AWS_REGION = "AWS_REGION";
     public static final String DYNAMODB_UPDATE_EVENT_TOPIC = "PublicationService.Database.Update";
+    public static final String EVENTS_BUCKET = new Environment().readEnv("EVENTS_BUCKET");
+    public static final String DYNAMO_EVENTS_FOLDER_IN_BUCKET = "dynamoEvents";
     private final EventPublisher eventPublisher;
+    private final S3Client s3Client;
 
     @JacocoGenerated
     public DynamodbStreamToEventBridgeHandler() {
-        this(defaultEventBridgePublisher());
+        this(defaultS3Client(), defaultEventBridgePublisher());
     }
 
-    protected DynamodbStreamToEventBridgeHandler(EventPublisher eventPublisher) {
+    protected DynamodbStreamToEventBridgeHandler(S3Client s3Client, EventPublisher eventPublisher) {
         this.eventPublisher = eventPublisher;
+        this.s3Client = s3Client;
     }
 
     @Override
-    public Void handleRequest(DynamodbEvent event, Context context) {
+    public URI handleRequest(DynamodbEvent event, Context context) {
+        URI savedFile = storeEventInS3(event);
         eventPublisher.publish(event);
-        return null;
+        return savedFile;
+    }
+
+    @JacocoGenerated
+    private static S3Client defaultS3Client() {
+        return S3Driver.defaultS3Client().build();
     }
 
     @JacocoGenerated
@@ -80,5 +98,12 @@ public class DynamodbStreamToEventBridgeHandler implements RequestHandler<Dynamo
                                        .build())
             .httpClientBuilder(UrlConnectionHttpClient.builder())
             .build();
+    }
+
+    private URI storeEventInS3(DynamodbEvent event) {
+        var s3Driver = new S3Driver(s3Client, EVENTS_BUCKET);
+        var json = attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(event)).orElseThrow();
+        var filename = UnixPath.of(DYNAMO_EVENTS_FOLDER_IN_BUCKET, UUID.randomUUID().toString());
+        return attempt(() -> s3Driver.insertFile(filename, json)).orElseThrow();
     }
 }
--- publication-event-handlers/src/test/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandlerTest.java
+++ publication-event-handlers/src/test/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandlerTest.java
@@ -1,37 +1,81 @@
 package no.unit.nva.publication.events.handlers.dynamodbstream;
 
-import com.amazonaws.services.lambda.runtime.Context;
+import static no.unit.nva.publication.events.handlers.dynamodbstream.DynamodbStreamToEventBridgeHandler.EVENTS_BUCKET;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
+import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.s3.S3Driver;
+import no.unit.nva.stubs.FakeContext;
+import no.unit.nva.stubs.FakeS3Client;
+import nva.commons.core.SingletonCollector;
 import nva.commons.core.ioutils.IoUtils;
+import nva.commons.core.paths.UriWrapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class DynamodbStreamToEventBridgeHandlerTest {
 
     public static final String DYNAMODB_STREAM_EVENT =
         IoUtils.stringFromResources(Path.of("dynamodbstreams/event.json"));
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private EventPublisher eventPublisher;
+    private FakeS3Client s3Client;
+    private FakeContext context;
+    private DynamodbStreamToEventBridgeHandler handler;
+
+    @BeforeEach
+    public void init() {
+        this.s3Client = new FakeS3Client();
+        this.context = new FakeContext();
+        this.eventPublisher = publisherThatPrintsToConsole();
+        this.handler = new DynamodbStreamToEventBridgeHandler(s3Client, eventPublisher);
+    }
+
+    //TODO this pre-existing test does nothng. Should be deleted after the transformation
+    @Test
+    void handleRequestWritesToConsoleOnValidEvent() throws Exception {
+        var event = objectMapper.readValue(DYNAMODB_STREAM_EVENT, DynamodbEvent.class);
+        handler.handleRequest(event, context);
+    }
 
     @Test
-    public void handleRequestWritesToConsoleOnValidEvent() throws Exception {
-        EventPublisher eventPublisher = event -> {
+    void shouldWriteDynamoDbStreamEventInS3Bucket() throws JsonProcessingException {
+        var event = parseDynamoDbEvent(DYNAMODB_STREAM_EVENT);
+        var savedFile = handler.handleRequest(event, context);
+        var savedEvent = readEventSavedInS3(savedFile);
+        var expectedEventId = event.getRecords().stream().collect(SingletonCollector.collect()).getEventID();
+        var actualEventId = savedEvent.getRecords().stream().collect(SingletonCollector.collect()).getEventID();
+        assertThat(actualEventId, is(equalTo(expectedEventId)));
+    }
+
+    private DynamodbEvent readEventSavedInS3(URI savedFile) throws JsonProcessingException {
+        var s3Driver = new S3Driver(s3Client, EVENTS_BUCKET);
+        var savedContent = s3Driver.getFile(UriWrapper.fromUri(savedFile).toS3bucketPath());
+        return parseDynamoDbEvent(savedContent);
+    }
+
+    private DynamodbEvent parseDynamoDbEvent(String savedContent) throws JsonProcessingException {
+        return JsonUtils.dtoObjectMapper.readValue(savedContent, DynamodbEvent.class);
+    }
+
+    private EventPublisher publisherThatPrintsToConsole() {
+        return event -> {
             try {
                 new ObjectMapper().writeValue(System.out, event);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         };
-
-        DynamodbStreamToEventBridgeHandler handler = createHandler(eventPublisher);
-        Context context = Mockito.mock(Context.class);
-        DynamodbEvent event = objectMapper.readValue(DYNAMODB_STREAM_EVENT, DynamodbEvent.class);
-        handler.handleRequest(event, context);
     }
 
     private DynamodbStreamToEventBridgeHandler createHandler(EventPublisher eventPublisher) {
-        return new DynamodbStreamToEventBridgeHandler(eventPublisher);
+        return new DynamodbStreamToEventBridgeHandler(s3Client, eventPublisher);
     }
 }
