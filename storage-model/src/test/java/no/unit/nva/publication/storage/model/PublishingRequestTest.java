da1d1386c0ff817371009222f79d422e65c8b60c
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-07-13 14:45:14 +0200

    remove duplicated code

--- storage-model/src/test/java/no/unit/nva/publication/storage/model/PublishingRequestTest.java
+++ storage-model/src/test/java/no/unit/nva/publication/storage/model/PublishingRequestTest.java
@@ -1,14 +1,11 @@
 package no.unit.nva.publication.storage.model;
 
-import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValues;
-import static no.unit.nva.testutils.RandomDataGenerator.randomElement;
-import static no.unit.nva.testutils.RandomDataGenerator.randomInstant;
+import static no.unit.nva.publication.StorageModelTestUtils.randomPublishingRequest;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import java.time.Instant;
 import no.unit.nva.identifiers.SortableIdentifier;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,7 +13,7 @@ import org.junit.jupiter.api.Test;
 class PublishingRequestTest {
 
     static SortableIdentifier randomIdentifier;
-    private final PublishingRequestCase sampleRequest = samplePublishingRequest();
+    private final PublishingRequestCase sampleRequest = randomPublishingRequest();
 
     @BeforeAll
     static void beforeAll() {
@@ -46,20 +43,5 @@ class PublishingRequestTest {
         assertThat(objectForCreatingNewEntry.getResourceIdentifier(), is(equalTo(publicationIdentifier)));
         assertThat(objectForCreatingNewEntry.getOwner(), is(equalTo(userInstance.getUserIdentifier())));
         assertThat(objectForCreatingNewEntry.getCustomerId(), is(equalTo(userInstance.getOrganizationUri())));
-    }
-
-    private PublishingRequestCase samplePublishingRequest() {
-        var userInstance = UserInstance.create(randomString(), randomUri());
-        var sample = new PublishingRequestCase();
-        sample.setCreatedDate(randomInstant());
-        sample.setModifiedDate(randomInstant());
-        sample.setRowVersion(randomString());
-        sample.setStatus(randomElement(PublishingRequestStatus.values()));
-        sample.setIdentifier(SortableIdentifier.next());
-        sample.setResourceIdentifier(SortableIdentifier.next());
-        sample.setCustomerId(randomUri());
-        sample.setOwner(randomString());
-        assertThat(sample,doesNotHaveEmptyValues());
-        return sample;
     }
 }
