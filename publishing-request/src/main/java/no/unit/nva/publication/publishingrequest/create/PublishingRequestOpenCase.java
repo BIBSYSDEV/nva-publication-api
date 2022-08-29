f023a60f44464c51a1314f4dba1bfee4a74ef4a5
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-08-29 12:02:41 +0200

    remove obsolete code

--- publishing-request/src/main/java/no/unit/nva/publication/publishingrequest/create/PublishingRequestOpenCase.java
+++
@@ -1,7 +0,0 @@
-package no.unit.nva.publication.publishingrequest.create;
-
-import no.unit.nva.publication.publishingrequest.PublishingRequestCaseDtoInterface;
-
-public class PublishingRequestOpenCase implements PublishingRequestCaseDtoInterface {
-
-}
