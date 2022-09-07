bfc624d495aff5897689726ed2bb05535d59a3bc
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-09-07 12:46:31 +0200

    GetMessageHandler refactoring

--- messages/src/main/java/no/unit/nva/publication/messages/read/GetMessageHandler.java
+++ messages/src/main/java/no/unit/nva/publication/messages/read/GetMessageHandler.java
@@ -9,6 +9,7 @@ import static no.unit.nva.publication.messages.MessageApiConfig.TICKET_IDENTIFIE
 import static no.unit.nva.publication.messages.MessageApiConfig.TICKET_PATH;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
+import java.net.URI;
 import java.util.Map;
 import no.unit.nva.identifiers.SortableIdentifier;
 import nva.commons.apigateway.ApiGatewayHandler;
@@ -25,16 +26,20 @@ public class GetMessageHandler extends ApiGatewayHandler<Void, Void> {
     
     @Override
     protected Void processInput(Void input, RequestInfo requestInfo, Context context) {
-        var resourceIdentifier = extractIdentifier(requestInfo, PUBLICATION_IDENTIFIER_PATH_PARAMETER);
-        var ticketIdentifier = extractIdentifier(requestInfo, TICKET_IDENTIFIER_PATH_PARAMETER);
-        var redirectUri = UriWrapper.fromHost(API_HOST)
-                              .addChild(PUBLICATION_PATH)
-                              .addChild(resourceIdentifier.toString())
-                              .addChild(TICKET_PATH)
-                              .addChild(ticketIdentifier.toString())
-                              .getUri();
+        var redirectUri = constructAssociatedTicketUri(requestInfo);
         addAdditionalHeaders(() -> Map.of(LOCATION_HEADER, redirectUri.toString()));
         return null;
+    }
+    
+    private URI constructAssociatedTicketUri(RequestInfo requestInfo) {
+        var resourceIdentifier = extractIdentifier(requestInfo, PUBLICATION_IDENTIFIER_PATH_PARAMETER);
+        var ticketIdentifier = extractIdentifier(requestInfo, TICKET_IDENTIFIER_PATH_PARAMETER);
+        return UriWrapper.fromHost(API_HOST)
+                   .addChild(PUBLICATION_PATH)
+                   .addChild(resourceIdentifier.toString())
+                   .addChild(TICKET_PATH)
+                   .addChild(ticketIdentifier.toString())
+                   .getUri();
     }
     
     @Override
