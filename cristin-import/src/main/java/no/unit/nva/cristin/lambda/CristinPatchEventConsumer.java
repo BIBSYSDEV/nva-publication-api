12b4682130c7214354df586f40388cb17bf84873
anetteOlli <anette.olli.siiri@sikt.no>
2023-01-13 14:58:54 +0100

    NP-28066-Cristin-import-post-processing-lambda-lookup-cristin-id, test outlines wip

--- cristin-import/src/main/java/no/unit/nva/cristin/lambda/CristinPatchEventConsumer.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/lambda/CristinPatchEventConsumer.java
@@ -1,23 +1,86 @@
 package no.unit.nva.cristin.lambda;
 
+import static no.unit.nva.publication.PublicationServiceConfig.defaultDynamoDbClient;
+import static no.unit.nva.publication.s3imports.ApplicationConstants.defaultS3Client;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.time.Clock;
 import no.unit.nva.events.handlers.EventHandler;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
 import no.unit.nva.events.models.EventReference;
 import no.unit.nva.model.Publication;
+import no.unit.nva.publication.s3imports.FileContentsEvent;
+import no.unit.nva.publication.service.impl.ResourceService;
+import no.unit.nva.s3.S3Driver;
+import nva.commons.core.JacocoGenerated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.S3Client;
 
 public class CristinPatchEventConsumer extends EventHandler<EventReference, Publication> {
 
     public static final String TOPIC = "PublicationService.DataImport.Filename";
     public static final String SUBTOPIC = "PublicationService.CristinData.PatchEntry";
 
+    public static final String WRONG_SUBTOPIC_ERROR_TEMPLATE =
+        "Unexpected subtopic: %s. Expected subtopic is: %s.";
+
+    private static final Clock CLOCK = Clock.systemDefaultZone();
+
+    private static final Logger logger = LoggerFactory.getLogger(CristinPatchEventConsumer.class);
+
+    private final ResourceService resourceService;
+    private final S3Client s3Client;
+
+    @JacocoGenerated
     public CristinPatchEventConsumer() {
+        this(defaultDynamoDbClient(), defaultS3Client());
+    }
+
+    @JacocoGenerated
+    protected CristinPatchEventConsumer(AmazonDynamoDB dynamoDbClient, S3Client s3Client) {
+        this(new ResourceService(dynamoDbClient, CLOCK), s3Client);
+    }
+
+    public CristinPatchEventConsumer(ResourceService resourceService, S3Client s3Client) {
         super(EventReference.class);
+        this.resourceService = resourceService;
+        this.s3Client = s3Client;
     }
 
     @Override
     protected Publication processInput(EventReference input, AwsEventBridgeEvent<EventReference> event,
                                        Context context) {
+        validateEvent(event);
+        var eventBody = readEventBody(input);
         return null;
+    }
+
+    private FileContentsEvent<JsonNode> readEventBody(EventReference input) {
+        var s3Driver = new S3Driver(s3Client, input.extractBucketName());
+        var json = s3Driver.readEvent(input.getUri());
+        return FileContentsEvent.fromJson(json, JsonNode.class);
+    }
+
+    private void validateEvent(AwsEventBridgeEvent<EventReference> event) {
+        if (!TOPIC.equalsIgnoreCase(event.getDetail().getTopic())) {
+            String errorMessage = messageIndicatingTheCorrectTopicType(event);
+            logger.info(event.toJsonString());
+            throw new IllegalArgumentException(errorMessage);
+        }
+        if (!SUBTOPIC.equalsIgnoreCase(event.getDetail().getSubtopic())) {
+            String errorMessage = messageIndicatingTheCorrectSubtopicType(event);
+            logger.info(event.toJsonString());
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private String messageIndicatingTheCorrectTopicType(AwsEventBridgeEvent<EventReference> event) {
+        return String.format("Unexpected topic: %s. Expected topic is: %s", event.getDetail().getTopic(), TOPIC);
+    }
+
+    private String messageIndicatingTheCorrectSubtopicType(AwsEventBridgeEvent<EventReference> event) {
+        return String.format(WRONG_SUBTOPIC_ERROR_TEMPLATE, event.getDetail().getSubtopic(), SUBTOPIC);
     }
 }
--- cristin-import/src/test/java/no/unit/nva/cristin/lambda/CristinPatchEventConsumerTest.java
+++ cristin-import/src/test/java/no/unit/nva/cristin/lambda/CristinPatchEventConsumerTest.java
@@ -5,24 +5,36 @@ import static no.unit.nva.cristin.lambda.CristinPatchEventConsumer.TOPIC;
 import static no.unit.nva.publication.s3imports.S3ImportsConfig.s3ImportsMapper;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static nva.commons.core.attempt.Try.attempt;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.time.Clock;
 import java.time.Instant;
-import no.unit.nva.cristin.AbstractCristinImportTest;
+import java.util.Set;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
 import no.unit.nva.events.models.EventReference;
+import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.model.AdditionalIdentifier;
+import no.unit.nva.model.Publication;
+import no.unit.nva.model.testing.PublicationGenerator;
+import no.unit.nva.publication.model.business.Resource;
+import no.unit.nva.publication.model.business.UserInstance;
+import no.unit.nva.publication.service.ResourcesLocalTest;
+import no.unit.nva.publication.service.impl.ResourceService;
 import no.unit.nva.s3.S3Driver;
 import no.unit.nva.stubs.FakeS3Client;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.core.ioutils.IoUtils;
 import nva.commons.core.paths.UnixPath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-public class CristinPatchEventConsumerTest extends AbstractCristinImportTest {
+public class CristinPatchEventConsumerTest extends ResourcesLocalTest {
 
     public static final Context CONTEXT = mock(Context.class);
 
@@ -31,22 +43,107 @@ public class CristinPatchEventConsumerTest extends AbstractCristinImportTest {
 
     private CristinPatchEventConsumer handler;
 
+    private ResourceService resourceService;
+
     private ByteArrayOutputStream outputStream;
 
     @BeforeEach
     public void init() {
         super.init();
+        resourceService = new ResourceService(super.client, Clock.systemDefaultZone());
         s3Client = new FakeS3Client();
         s3Driver = new S3Driver(s3Client, "ignored");
-        handler = new CristinPatchEventConsumer();
+        handler = new CristinPatchEventConsumer(resourceService, s3Client);
         outputStream = new ByteArrayOutputStream();
     }
 
     @Test
-    void dummyTest() throws IOException {
+    void shouldThrowExceptionWhenPublicationCannotBeRetrieved() {
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSearchingForNvaPublicationByCristinIdentifierReturnsMoreThanOnePublication()
+        throws ApiGatewayException, IOException {
+        var childPublication = createPersistedPublicationWithStatusPublishedWithSpecifiedCristinId(randomString());
+        var partOfCristinId = randomString();
+        persistSeveralPublicationsWithTheSameCristinId(partOfCristinId);
+        var partOfEventReference = createPartOfEventReference(childPublication.getIdentifier(), partOfCristinId);
+        var fileUri = s3Driver.insertFile(randomPath(), partOfEventReference);
+        var eventReference = createInputEventForFile(fileUri);
+    }
+
+    private void persistSeveralPublicationsWithTheSameCristinId(String cristinId) throws ApiGatewayException {
+        createPersistedPublicationWithStatusPublishedWithSpecifiedCristinId(cristinId);
+        createPersistedPublicationWithStatusPublishedWithSpecifiedCristinId(cristinId);
+    }
+
+    private String createPartOfEventReference(SortableIdentifier identifier, String partOfCristinId) {
+        return null;
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSearchingForNvaPublicationByCristinIdentifierReturnsNoPublicatiosn() {
+        //blah blah
+    }
+
+    @Test
+    void shouldPersistNvaPublicationIdentifierIfSuccessFulLookup() {
+        // blah blah
+    }
+
+    @Test
+    void shouldLogErrorWhenFailingToStorePublicationToDynamo() {
+        //blah blah
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSubtopicIsNotAsExpected() throws IOException {
         var fileUri = s3Driver.insertFile(randomPath(), randomString());
-        var input = toInputStream(createInputEventForFile(fileUri));
-        handler.handleRequest(input, outputStream, CONTEXT);
+        var eventReferenceWithInvalidSubtopic = createEventWithInvalidSubtopic(fileUri);
+        var input = toInputStream(eventReferenceWithInvalidSubtopic);
+        Executable action = () -> handler.handleRequest(input, outputStream, CONTEXT);
+        assertThrows(IllegalArgumentException.class, action);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTopicIsNotAsExpected() throws IOException {
+        var fileUri = s3Driver.insertFile(randomPath(), randomString());
+        var eventReferenceWithInvalidTopic = createEventWithInvalidTopic(fileUri);
+        var input = toInputStream(eventReferenceWithInvalidTopic);
+        Executable action = () -> handler.handleRequest(input, outputStream, CONTEXT);
+        assertThrows(IllegalArgumentException.class, action);
+    }
+
+    private static AwsEventBridgeEvent<EventReference> createEventWithInvalidTopic(URI fileUri) {
+        var eventReference = new EventReference(randomString(), SUBTOPIC, fileUri);
+        var request = new AwsEventBridgeEvent<EventReference>();
+
+        request.setDetail(eventReference);
+        return request;
+    }
+
+    private Publication createPersistedPublicationWithStatusPublishedWithSpecifiedCristinId(String cristinId)
+        throws ApiGatewayException {
+        Publication publication = PublicationGenerator.randomPublication();
+        publication.setAdditionalIdentifiers(createAdditionalIdentifiersWithCristinId(cristinId));
+        UserInstance userInstance = UserInstance.fromPublication(publication);
+        SortableIdentifier publicationIdentifier =
+            Resource.fromPublication(publication).persistNew(resourceService, userInstance).getIdentifier();
+        resourceService.publishPublication(userInstance, publicationIdentifier);
+        return resourceService.getPublicationByIdentifier(publicationIdentifier);
+    }
+
+    private Set<AdditionalIdentifier> createAdditionalIdentifiersWithCristinId(String cristinId) {
+        return Set.of(new AdditionalIdentifier("Cristin", cristinId));
+    }
+
+    private AwsEventBridgeEvent<EventReference> createEventWithInvalidSubtopic(URI fileUri) {
+        var eventReference = new EventReference(TOPIC, randomString(), fileUri);
+        var request = new AwsEventBridgeEvent<EventReference>();
+
+        request.setDetail(eventReference);
+        return request;
     }
 
     private UnixPath randomPath() {
