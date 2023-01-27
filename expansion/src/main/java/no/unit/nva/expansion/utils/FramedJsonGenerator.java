593e887cf743ed0f08fd0338686791a496c95ec1
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-01-27 14:15:22 +0100

    Revise model building

--- expansion/src/main/java/no/unit/nva/expansion/utils/FramedJsonGenerator.java
+++ expansion/src/main/java/no/unit/nva/expansion/utils/FramedJsonGenerator.java
@@ -1,11 +1,10 @@
 package no.unit.nva.expansion.utils;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import static java.util.Objects.isNull;
 import com.github.jsonldjava.core.JsonLdOptions;
+import java.io.InputStream;
+import java.util.List;
 import nva.commons.core.JacocoGenerated;
-import nva.commons.core.attempt.Try;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.Model;
@@ -15,21 +14,12 @@ import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.RDFWriter;
+import org.apache.jena.riot.RiotException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-
-import static no.unit.nva.expansion.ExpansionConfig.objectMapper;
-import static nva.commons.core.attempt.Try.attempt;
-
 @JacocoGenerated
 public class FramedJsonGenerator {
-
-    public static final String JSON_LD_GRAPH = "@graph";
     private static final Logger logger = LoggerFactory.getLogger(FramedJsonGenerator.class);
     private final Model model;
     private final String framedJson;
@@ -44,23 +34,20 @@ public class FramedJsonGenerator {
     }
 
     private Model createModel(List<InputStream> streams) {
-        ObjectNode document = objectMapper.createObjectNode();
-        ArrayNode graph = objectMapper.createArrayNode();
-        streams.stream()
-                .map(attempt(objectMapper::readTree))
-                .filter(this::keepSuccessesAndLogErrors)
-                .map(Try::orElseThrow)
-                .forEach(graph::add);
-
-        document.set(JSON_LD_GRAPH, graph);
-
-        var defaultModel = ModelFactory.createDefaultModel();
-        updateModel(document.toString(), defaultModel);
-        return addTopLevelAffiliation(defaultModel);
+        var m = ModelFactory.createDefaultModel();
+        streams.forEach(s -> loadDataIntoModel(m, s));
+        return addTopLevelAffiliation(m);
     }
 
-    private static void updateModel(String update, Model oldModel) {
-        RDFDataMgr.read(oldModel, new ByteArrayInputStream(update.getBytes(StandardCharsets.UTF_8)), Lang.JSONLD);
+    private void loadDataIntoModel(Model model, InputStream inputStream) {
+        if (isNull(inputStream)) {
+            return;
+        }
+        try {
+            RDFDataMgr.read(model, inputStream, Lang.JSONLD);
+        } catch (RiotException e) {
+            logInvalidJsonLdInput(e);
+        }
     }
 
     private Model addTopLevelAffiliation(Model model) {
@@ -99,14 +86,7 @@ public class FramedJsonGenerator {
         return jsonLdOptions;
     }
 
-    private boolean keepSuccessesAndLogErrors(Try<JsonNode> jsonNodeTry) {
-        if (jsonNodeTry.isFailure()) {
-            logFramingFailure(jsonNodeTry.getException());
-        }
-        return jsonNodeTry.isSuccess();
-    }
-
-    private void logFramingFailure(Exception exception) {
-        logger.warn("Framing failed:", exception);
+    private static void logInvalidJsonLdInput(Exception exception) {
+        logger.warn("Invalid JSON LD input encountered: ", exception);
     }
 }
