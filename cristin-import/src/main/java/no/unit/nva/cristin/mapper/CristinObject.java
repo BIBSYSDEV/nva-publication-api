dd4685635134bd9eb4283e1f578f59863d45965d
anetteOlli <anette.olli.siiri@sikt.no>
2023-01-30 15:46:11 +0100

    NP-32264-cristin-import-ignore-new-field, added more fields to ignore FINANSIERING_VARBEID

--- cristin-import/src/main/java/no/unit/nva/cristin/mapper/CristinObject.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/mapper/CristinObject.java
@@ -31,7 +31,7 @@ import no.unit.nva.model.Publication;
     "dato_utgitt", "finansiering_varbeid", "type_produkt",
     "kildepostid", "eierkode_opprettet", "arkivpost", "VITENSKAPELIGARBEID_LOKAL",
     "type_kunstneriskproduksjon", "type_utstilling", "pubidnr", "eierkode_siste_endring",
-    "VARBEID_VDISIPLIN",
+    "VARBEID_VDISIPLIN", "FINANSIERING_VARBEID",
     "varbeid_vdisiplin", "arkivfil", "vitenskapeligarbeid_lokal", "merknadtekst", "h_dbh_forskres_publikasjon"})
 
 @SuppressWarnings({"PMD.TooManyFields"})
