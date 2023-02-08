936ad3c10af74b5c2d92f5fed8f4f908ec752af3
anetteOlli <anette.olli.siiri@sikt.no>
2023-02-08 14:07:32 +0100

    NP-28067-cristin-import-update-child-publication, implemented

---
+++ cristin-import/src/main/java/no/unit/nva/cristin/patcher/CristinPatcher.java
@@ -0,0 +1,48 @@
+package no.unit.nva.cristin.patcher;
+
+import static no.unit.nva.cristin.lambda.constants.MappingConstants.NVA_API_DOMAIN;
+import java.net.URI;
+import no.unit.nva.cristin.patcher.exception.PublicationInstanceMismatchException;
+import no.unit.nva.cristin.patcher.model.ParentAndChild;
+import no.unit.nva.model.Publication;
+import no.unit.nva.model.contexttypes.Chapter;
+import no.unit.nva.model.contexttypes.PublicationContext;
+import no.unit.nva.model.instancetypes.PublicationInstance;
+import no.unit.nva.model.instancetypes.book.BookAnthology;
+import nva.commons.core.paths.UriWrapper;
+
+public final class CristinPatcher {
+
+    private static final String CHILD_PARENT_REFERENCE_MISMATCH =
+        "Invalid child / parent reference mismatch, child publicationContext: %s, parent publicationInstance: %s";
+
+    private CristinPatcher() {
+
+    }
+
+    public static ParentAndChild updateChildPublication(ParentAndChild parentAndChild) {
+        var childContext =
+            parentAndChild.getChildPublication().getEntityDescription().getReference().getPublicationContext();
+        var parentPublicationInstance =
+            parentAndChild.getParentPublication().getEntityDescription().getReference().getPublicationInstance();
+        if (parentChildPublicationContextMatch(childContext, parentPublicationInstance)) {
+            var chapter = (Chapter) childContext;
+            chapter.setPartOf(createPartOfUri(parentAndChild.getParentPublication()));
+        } else {
+            throw new PublicationInstanceMismatchException(
+                String.format(CHILD_PARENT_REFERENCE_MISMATCH,
+                              childContext.getClass().getSimpleName(),
+                              parentPublicationInstance.getClass().getSimpleName()));
+        }
+        return parentAndChild;
+    }
+
+    private static boolean parentChildPublicationContextMatch(PublicationContext childContext,
+                                                              PublicationInstance parentPublicationInstance) {
+        return childContext instanceof Chapter && parentPublicationInstance instanceof BookAnthology;
+    }
+
+    private static URI createPartOfUri(Publication parentPublication) {
+        return UriWrapper.fromUri(NVA_API_DOMAIN + "/" + parentPublication.getIdentifier()).getUri();
+    }
+}
---
+++ cristin-import/src/main/java/no/unit/nva/cristin/patcher/exception/PublicationInstanceMismatchException.java
@@ -0,0 +1,8 @@
+package no.unit.nva.cristin.patcher.exception;
+
+public class PublicationInstanceMismatchException extends RuntimeException {
+
+    public PublicationInstanceMismatchException(String message) {
+        super(message);
+    }
+}
