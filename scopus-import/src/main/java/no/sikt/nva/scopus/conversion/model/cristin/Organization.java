5e33a392ad20eaa2c48a5c59a78f052c297e085d
Kir Truhacev <kir.truhacev@sikt.no>
2023-02-17 14:30:37 +0100

    NP-Fixes-after-scopus-kjøring, work in progress

--- scopus-import/src/main/java/no/sikt/nva/scopus/conversion/model/cristin/Organization.java
+++ scopus-import/src/main/java/no/sikt/nva/scopus/conversion/model/cristin/Organization.java
@@ -1,13 +1,10 @@
 package no.sikt.nva.scopus.conversion.model.cristin;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.net.URI;
 import java.util.Map;
 
-@SuppressWarnings("PMD.UnnecessaryAnnotationValueElement")
-@JsonIgnoreProperties(value = {"type", "@context", "hasPart"})
 public class Organization {
 
     @JsonProperty("id")
