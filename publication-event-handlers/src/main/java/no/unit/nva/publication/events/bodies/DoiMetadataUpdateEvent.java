245c1bd204ac5482245a2d2c50846e14074ee81f
anetteOlli <anette.olli.siiri@sikt.no>
2023-03-17 14:27:41 +0100

    removing publication from event

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/bodies/DoiMetadataUpdateEvent.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/bodies/DoiMetadataUpdateEvent.java
@@ -17,15 +17,13 @@ import nva.commons.core.paths.UriWrapper;
 
 public class DoiMetadataUpdateEvent {
 
-    public static final String REQUEST_DRAFT_DOI_EVENT_TOPIC = "PublicationService.Doi.CreationRequest";
     public static final String UPDATE_DOI_EVENT_TOPIC = "PublicationService.Doi.UpdateRequest";
     public static final String DELETE_DRAFT_DOI_EVENT_TOPIC = "PublicationService.Doi.DeleteDraftRequest";
     public static final String TOPIC = "topic";
     public static final String PUBLICATION_ID = "publicationId";
     public static final String CUSTOMER_ID = "customerId";
     public static final String DOI = "doi";
-
-    public static final String ITEM = "item";
+    
     protected static final String EMPTY_EVENT_TOPIC = "empty";
     @JsonProperty(TOPIC)
     private final String topic;
@@ -39,20 +37,14 @@ public class DoiMetadataUpdateEvent {
     @JsonProperty(DOI)
     private final URI doi;
 
-    @Deprecated
-    @JsonProperty(ITEM)
-    private final Publication item;
-
     @JacocoGenerated
     @JsonCreator
     public DoiMetadataUpdateEvent(
         @JsonProperty(TOPIC) String type,
         @JsonProperty(PUBLICATION_ID) URI publicationId,
         @JsonProperty(CUSTOMER_ID) URI customerId,
-        @JsonProperty(ITEM) Publication publication,
         @JsonProperty(DOI) URI doi) {
         this.topic = type;
-        this.item = publication;
         this.publicationId = publicationId;
         this.customerId = customerId;
         this.doi = doi;
@@ -62,7 +54,7 @@ public class DoiMetadataUpdateEvent {
         URI publicationId = inferPublicationId(newEntry);
         URI customerId = extractCustomerId(newEntry);
         URI doi = extractDoi(newEntry);
-        return new DoiMetadataUpdateEvent(UPDATE_DOI_EVENT_TOPIC, publicationId, customerId, newEntry, doi);
+        return new DoiMetadataUpdateEvent(UPDATE_DOI_EVENT_TOPIC, publicationId, customerId, doi);
     }
 
     public static DoiMetadataUpdateEvent createDeleteDraftDoiEvent(DoiRequest doiRequest,
@@ -71,11 +63,11 @@ public class DoiMetadataUpdateEvent {
         URI publicationId = inferPublicationId(publication);
         URI customerId = extractCustomerId(publication);
         URI doi = extractDoi(publication);
-        return new DoiMetadataUpdateEvent(DELETE_DRAFT_DOI_EVENT_TOPIC, publicationId, customerId, publication, doi);
+        return new DoiMetadataUpdateEvent(DELETE_DRAFT_DOI_EVENT_TOPIC, publicationId, customerId, doi);
     }
 
     public static DoiMetadataUpdateEvent empty() {
-        return new DoiMetadataUpdateEvent(EMPTY_EVENT_TOPIC, null, null, null, null);
+        return new DoiMetadataUpdateEvent(EMPTY_EVENT_TOPIC, null, null, null);
     }
 
     @JacocoGenerated
@@ -118,11 +110,6 @@ public class DoiMetadataUpdateEvent {
     @JacocoGenerated
     public URI getDoi() {
         return doi;
-    }
-
-    @JacocoGenerated
-    public Publication getItem() {
-        return item;
     }
 
     public String toJsonString() {
