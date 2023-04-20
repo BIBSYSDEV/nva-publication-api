fb408b7cbe2e3e02572085d0edefd535f8f01f6c
monaullah <monaullah98@gmail.com>
2023-04-20 13:00:15 +0200

    Handler fetches ticket messages when creating ticket dtos

--- tickets/src/main/java/no/unit/nva/publication/ticket/read/ListTicketsForPublicationHandler.java
+++ tickets/src/main/java/no/unit/nva/publication/ticket/read/ListTicketsForPublicationHandler.java
@@ -3,11 +3,14 @@ package no.unit.nva.publication.ticket.read;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import java.net.HttpURLConnection;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.publication.model.business.TicketEntry;
 import no.unit.nva.publication.model.business.UserInstance;
 import no.unit.nva.publication.service.impl.ResourceService;
+import no.unit.nva.publication.service.impl.TicketService;
 import no.unit.nva.publication.ticket.TicketDto;
 import no.unit.nva.publication.ticket.TicketHandler;
 import nva.commons.apigateway.AccessRight;
@@ -20,15 +23,18 @@ import nva.commons.core.JacocoGenerated;
 public class ListTicketsForPublicationHandler extends TicketHandler<Void, TicketCollection> {
     
     private final ResourceService resourceService;
+
+    private final TicketService ticketService;
     
     @JacocoGenerated
     public ListTicketsForPublicationHandler() {
-        this(ResourceService.defaultService());
+        this(ResourceService.defaultService(), TicketService.defaultService());
     }
     
-    public ListTicketsForPublicationHandler(ResourceService resourceService) {
+    public ListTicketsForPublicationHandler(ResourceService resourceService, TicketService ticketService) {
         super(Void.class);
         this.resourceService = resourceService;
+        this.ticketService = ticketService;
     }
     
     @Override
@@ -38,7 +44,7 @@ public class ListTicketsForPublicationHandler extends TicketHandler<Void, Ticket
         var userInstance = UserInstance.fromRequestInfo(requestInfo);
         
         var ticketDtos = fetchTickets(requestInfo, publicationIdentifier, userInstance);
-        return TicketCollection.fromTickets(ticketDtos.collect(Collectors.toList()));
+        return TicketCollection.fromTickets(ticketDtos);
     }
     
     @Override
@@ -50,32 +56,35 @@ public class ListTicketsForPublicationHandler extends TicketHandler<Void, Ticket
         return requestInfo.userIsAuthorized(AccessRight.APPROVE_DOI_REQUEST.toString());
     }
     
-    private Stream<TicketDto> fetchTickets(RequestInfo requestInfo,
-                                           SortableIdentifier publicationIdentifier,
-                                           UserInstance userInstance) throws ApiGatewayException {
-        return userIsAuthorizedToViewOtherUsersTickets(requestInfo)
+    private List<TicketDto> fetchTickets(RequestInfo requestInfo,
+                                         SortableIdentifier publicationIdentifier,
+                                         UserInstance userInstance) throws ApiGatewayException {
+        var ticketEntries = userIsAuthorizedToViewOtherUsersTickets(requestInfo)
                    ? fetchTicketsForElevatedUser(userInstance, publicationIdentifier)
                    : fetchTicketsForPublicationOwner(publicationIdentifier, userInstance);
+
+        return ticketEntries.map(this::createDto).collect(Collectors.toList());
     }
     
-    private Stream<TicketDto> fetchTicketsForPublicationOwner(SortableIdentifier publicationIdentifier,
+    private Stream<TicketEntry> fetchTicketsForPublicationOwner(SortableIdentifier publicationIdentifier,
                                                               UserInstance userInstance)
         throws ApiGatewayException {
         
-        var tickets = attempt(
-            () -> resourceService.fetchAllTicketsForPublication(userInstance, publicationIdentifier))
+        return attempt(() -> resourceService.fetchAllTicketsForPublication(userInstance, publicationIdentifier))
                           .orElseThrow(fail -> handleFetchingError(fail.getException()));
-        return tickets.map(TicketDto::fromTicket);
     }
     
-    private Stream<TicketDto> fetchTicketsForElevatedUser(UserInstance userInstance,
+    private Stream<TicketEntry> fetchTicketsForElevatedUser(UserInstance userInstance,
                                                           SortableIdentifier publicationIdentifier)
         throws ApiGatewayException {
         
-        var tickets =
-            attempt(() -> resourceService.fetchAllTicketsForElevatedUser(userInstance, publicationIdentifier))
+        return attempt(() -> resourceService.fetchAllTicketsForElevatedUser(userInstance, publicationIdentifier))
                 .orElseThrow(fail -> handleFetchingError(fail.getException()));
-        return tickets.map(TicketDto::fromTicket);
+    }
+
+    private TicketDto createDto(TicketEntry ticket) {
+        var messages = ticket.fetchMessages(ticketService);
+        return TicketDto.fromTicket(ticket, messages);
     }
     
     private ApiGatewayException handleFetchingError(Exception exception) {
