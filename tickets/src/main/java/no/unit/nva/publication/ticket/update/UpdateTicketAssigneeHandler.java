9f75c9a45cfe4694b1cc52820f3fc4fb5fbdaddd
Eyob Teweldemedhin <eyob.teweldemedhin@sikt.no>
2023-04-25 21:01:11 +0200

    Fix merge conflicts

--- tickets/src/main/java/no/unit/nva/publication/ticket/update/UpdateTicketAssigneeHandler.java
+++ tickets/src/main/java/no/unit/nva/publication/ticket/update/UpdateTicketAssigneeHandler.java
@@ -1,19 +1,22 @@
 package no.unit.nva.publication.ticket.update;
 
+import static java.net.HttpURLConnection.HTTP_ACCEPTED;
 import com.amazonaws.services.lambda.runtime.Context;
-import java.net.HttpURLConnection;
+import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.publication.model.business.TicketEntry;
+import no.unit.nva.publication.model.business.UserInstance;
 import no.unit.nva.publication.service.impl.TicketService;
+import no.unit.nva.publication.ticket.TicketConfig;
 import no.unit.nva.publication.ticket.TicketDto;
-import no.unit.nva.publication.ticket.TicketHandler;
 import nva.commons.apigateway.AccessRight;
+import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.ForbiddenException;
 import nva.commons.apigateway.exceptions.UnauthorizedException;
 import nva.commons.core.JacocoGenerated;
 
-public class UpdateTicketAssigneeHandler extends TicketHandler<TicketDto, Void> {
+public class UpdateTicketAssigneeHandler extends ApiGatewayHandler<Void, TicketDto> {
 
     private final TicketService ticketService;
 
@@ -24,26 +27,30 @@ public class UpdateTicketAssigneeHandler extends TicketHandler<TicketDto, Void> 
     }
 
     public UpdateTicketAssigneeHandler(TicketService ticketService) {
-        super(TicketDto.class);
+        super(Void.class);
         this.ticketService = ticketService;
     }
 
     @Override
-    protected Void processInput(TicketDto input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+    protected TicketDto processInput(Void input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
         var ticketIdentifier = extractTicketIdentifierFromPath(requestInfo);
         var ticket = ticketService.fetchTicketByIdentifier(ticketIdentifier);
 
         if (userIsNotAuthorized(requestInfo, ticket)) {
             throw new ForbiddenException();
         }
-
-        ticketService.updateTicketAssignee(ticket, input.getAssignee());
+        var user = UserInstance.fromTicket(ticket);
+        ticketService.updateTicketAssignee(ticket, user.getUser());
         return null;
     }
 
     @Override
-    protected Integer getSuccessStatusCode(TicketDto input, Void output) {
-        return HttpURLConnection.HTTP_ACCEPTED;
+    protected Integer getSuccessStatusCode(Void input, TicketDto output) {
+        return HTTP_ACCEPTED;
+    }
+
+    private static SortableIdentifier extractTicketIdentifierFromPath(RequestInfo requestInfo) {
+        return new SortableIdentifier(requestInfo.getPathParameter(TicketConfig.TICKET_IDENTIFIER_PARAMETER_NAME));
     }
 
     private static boolean userIsNotAuthorized(RequestInfo requestInfo, TicketEntry ticket)
