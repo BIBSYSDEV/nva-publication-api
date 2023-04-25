82406cfa1f241b5a165b864ca041bde5015f7299
monaullah <monaullah98@gmail.com>
2023-04-25 14:37:29 +0200

    Added FetchPublicationContextHandler, test for content negotiation

---
+++ publication-rest/src/main/java/no/unit/nva/publication/fetch/FetchPublicationContextHandler.java
@@ -0,0 +1,37 @@
+package no.unit.nva.publication.fetch;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.common.net.MediaType;
+import nva.commons.apigateway.ApiGatewayHandler;
+import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
+import java.net.HttpURLConnection;
+import java.util.List;
+import static com.google.common.net.MediaType.JSON_UTF_8;
+import static nva.commons.apigateway.MediaTypes.APPLICATION_JSON_LD;
+
+public class FetchPublicationContextHandler extends ApiGatewayHandler<Void, String> {
+
+    public FetchPublicationContextHandler() {
+        super(Void.class);
+    }
+
+    @Override
+    protected String processInput(Void input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+        return null;
+    }
+
+    @Override
+    protected Integer getSuccessStatusCode(Void input, String output) {
+        return HttpURLConnection.HTTP_OK;
+    }
+
+    @Override
+    protected List<MediaType> listSupportedMediaTypes() {
+        return List.of(
+                APPLICATION_JSON_LD,
+                JSON_UTF_8
+        );
+    }
+
+}
---
+++ publication-rest/src/test/java/no/unit/nva/publication/fetch/FetchPublicationContextHandlerTest.java
@@ -0,0 +1,64 @@
+package no.unit.nva.publication.fetch;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import static com.google.common.net.HttpHeaders.ACCEPT;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.apigateway.GatewayResponse;
+import org.junit.jupiter.api.BeforeEach;
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.zalando.problem.Problem;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.Map;
+import java.util.stream.Stream;
+import static no.unit.nva.publication.PublicationRestHandlersTestConfig.restApiMapper;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.mock;
+
+class FetchPublicationContextHandlerTest {
+
+    private static final String TEXT_ANY = "text/*";
+    private static final String TEXT_HTML = "text/html";
+    private static final String APPLICATION_XHTML = "application/xhtml+xml";
+    private static final String UNSUPPORTED_ACCEPT_HEADER_MESSAGE = "contains no supported Accept header values.";
+    private FetchPublicationContextHandler fetchPublicationContextHandler;
+    private Context context;
+    private ByteArrayOutputStream output;
+
+    @BeforeEach
+    void setUp() {
+        context = mock(Context.class);
+        output = new ByteArrayOutputStream();
+        fetchPublicationContextHandler = new FetchPublicationContextHandler();
+    }
+
+    @ParameterizedTest(name = "mediaType {0} is invalid")
+    @MethodSource("unsupportedMediaTypes")
+    void shouldReturnUnsupportedMediaTypeIfRequestHeaderAcceptsAnythingOtherThanJsonOrJsonLdOrDefault(String mediaType)
+            throws IOException {
+        var request = generateHandlerRequest(Map.of(ACCEPT, mediaType));
+        fetchPublicationContextHandler.handleRequest(request, output, context);
+        var response = GatewayResponse.fromOutputStream(output, Problem.class);
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_UNSUPPORTED_TYPE)));
+        var problem = response.getBodyObject(Problem.class);
+        assertThat(problem.getDetail(), is(containsString(UNSUPPORTED_ACCEPT_HEADER_MESSAGE)));
+    }
+
+    private InputStream generateHandlerRequest(Map<String, String> headers)
+            throws JsonProcessingException {
+        return new HandlerRequestBuilder<InputStream>(restApiMapper)
+                .withHeaders(headers)
+                .build();
+    }
+
+    private static Stream<String> unsupportedMediaTypes() {
+        return Stream.of(TEXT_ANY, TEXT_HTML, APPLICATION_XHTML);
+    }
+}
