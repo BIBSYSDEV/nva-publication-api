e48f68161843bfdc4820181264b6eda23c3ef4b0
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2023-05-02 08:02:56 +0200

    finalizedBy added to ExpandedTicket (#1155)

--- expansion/src/main/java/no/unit/nva/expansion/model/ExpandedTicket.java
+++ expansion/src/main/java/no/unit/nva/expansion/model/ExpandedTicket.java
@@ -4,24 +4,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-import java.net.URI;
-import java.util.List;
-import java.util.Set;
 import no.unit.nva.expansion.ResourceExpansionService;
 import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.model.Username;
 import no.unit.nva.publication.PublicationServiceConfig;
 import no.unit.nva.publication.model.PublicationSummary;
 import no.unit.nva.publication.model.business.DoiRequest;
+import no.unit.nva.publication.model.business.PublishingRequestCase;
 import no.unit.nva.publication.model.business.GeneralSupportRequest;
 import no.unit.nva.publication.model.business.Message;
-import no.unit.nva.publication.model.business.PublishingRequestCase;
 import no.unit.nva.publication.model.business.TicketEntry;
-import no.unit.nva.publication.model.business.TicketStatus;
 import no.unit.nva.publication.model.business.User;
+import no.unit.nva.publication.model.business.TicketStatus;
 import no.unit.nva.publication.service.impl.ResourceService;
 import no.unit.nva.publication.service.impl.TicketService;
 import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.paths.UriWrapper;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Set;
 
 @JsonTypeInfo(use = Id.NAME, property = "type")
 @JsonSubTypes({
@@ -36,7 +38,7 @@ public abstract class ExpandedTicket implements ExpandedDataEntry {
     public static final String ID_FIELD = "id";
     public static final String VIEWED_BY_FIELD = "viewedBy";
     private static final String MESSAGES_FIELD = "messages";
-    
+    public static final String FINALIZED_BY_FIELD = "finalizedBy";
     @JsonProperty(ID_FIELD)
     private URI id;
     @JsonProperty(MESSAGES_FIELD)
@@ -45,12 +47,14 @@ public abstract class ExpandedTicket implements ExpandedDataEntry {
     private Set<User> viewedBy;
     @JsonProperty(PUBLICATION_FIELD)
     private PublicationSummary publication;
-    
+    @JsonProperty(FINALIZED_BY_FIELD)
+    private Username finalizedBy;
+
     public static ExpandedDataEntry create(TicketEntry ticketEntry,
                                            ResourceService resourceService,
                                            ResourceExpansionService expansionService,
                                            TicketService ticketService) throws NotFoundException {
-        
+
         if (ticketEntry instanceof DoiRequest) {
             return ExpandedDoiRequest.createEntry((DoiRequest) ticketEntry,
                 expansionService,
@@ -74,15 +78,23 @@ public abstract class ExpandedTicket implements ExpandedDataEntry {
         }
         throw new UnsupportedOperationException();
     }
-    
+
+    public Username getFinalizedBy() {
+        return finalizedBy;
+    }
+
+    public void setFinalizedBy(Username finalizedBy) {
+        this.finalizedBy = finalizedBy;
+    }
+
     public final Set<User> getViewedBy() {
         return viewedBy;
     }
-    
+
     public final void setViewedBy(Set<User> viewedBy) {
         this.viewedBy = viewedBy;
     }
-    
+
     @JsonProperty(PUBLICATION_FIELD)
     public final PublicationSummary getPublication() {
         return this.publication;
