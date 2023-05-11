023e67949895fa005c3be909d0bf761b35720162
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2023-05-11 14:05:02 +0200

    Np 44697 batch scan lambda to reindex import candidates (#1188)
    * openapi spec fix
    
    * work in progress
    
    * batch scan for import candidates

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/bodies/ScanDatabaseRequest.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/bodies/ScanDatabaseRequest.java
@@ -8,66 +8,69 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.time.Instant;
 import java.util.Map;
 import no.unit.nva.commons.json.JsonSerializable;
+import nva.commons.core.Environment;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
 
 public class ScanDatabaseRequest implements JsonSerializable {
-    
+
     public static final String START_MARKER = "startMarker";
     public static final String PAGE_SIZE = "pageSize";
-    public static final String SCAN_REQUEST_EVENT_TOPIC = "PublicationService.DataEntry.ScanAndUpdateRowVersion";
     public static final int DEFAULT_PAGE_SIZE = 700; // Choosing for safety 3/4 of max page size.
     public static final int MAX_PAGE_SIZE = 1000;
+    public static final String OUTPUT_EVENT_TOPIC = "OUTPUT_EVENT_TOPIC";
     @JsonProperty(START_MARKER)
     private final Map<String, AttributeValue> startMarker;
     @JsonProperty(PAGE_SIZE)
     private final int pageSize;
-    
+    private final String topic;
+
     @JsonCreator
     public ScanDatabaseRequest(@JsonProperty(PAGE_SIZE) int pageSize,
                                @JsonProperty(START_MARKER) Map<String, AttributeValue> startMarker) {
         this.pageSize = pageSize;
         this.startMarker = startMarker;
+        this.topic = new Environment().readEnv(OUTPUT_EVENT_TOPIC);
     }
-    
+
     public static ScanDatabaseRequest fromJson(String detail) throws JsonProcessingException {
         return objectMapper.readValue(detail, ScanDatabaseRequest.class);
     }
-    
+
     @JsonProperty("topic")
     public String getTopic() {
-        return SCAN_REQUEST_EVENT_TOPIC;
+        return topic;
     }
-    
+
     public int getPageSize() {
         return pageSizeWithinLimits(pageSize)
-                   ? pageSize
-                   : DEFAULT_PAGE_SIZE;
+                ? pageSize
+                : DEFAULT_PAGE_SIZE;
     }
-    
+
     public Map<String, AttributeValue> getStartMarker() {
         return startMarker;
     }
-    
+
     public ScanDatabaseRequest newScanDatabaseRequest(Map<String, AttributeValue> newStartMarker) {
         return new ScanDatabaseRequest(this.getPageSize(), newStartMarker);
     }
-    
+
     public PutEventsRequestEntry createNewEventEntry(
-        String eventBusName,
-        String detailType,
-        String invokedFunctionArn
+            String eventBusName,
+            String detailType,
+            String invokedFunctionArn
     ) {
         return PutEventsRequestEntry
-                   .builder()
-                   .eventBusName(eventBusName)
-                   .detail(this.toJsonString())
-                   .detailType(detailType)
-                   .resources(invokedFunctionArn)
-                   .time(Instant.now())
-                   .source(invokedFunctionArn)
-                   .build();
+                .builder()
+                .eventBusName(eventBusName)
+                .detail(this.toJsonString())
+                .detailType(detailType)
+                .resources(invokedFunctionArn)
+                .time(Instant.now())
+                .source(invokedFunctionArn)
+                .build();
     }
-    
+
     private boolean pageSizeWithinLimits(int pageSize) {
         return pageSize > 0 && pageSize <= MAX_PAGE_SIZE;
     }
