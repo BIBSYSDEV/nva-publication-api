21417cf5321db0b6c8b3f668ab97a0742a27a8d8
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2023-05-24 08:50:08 +0200

    Batch scan fix (#1207)
    * removing env_var from ScanDatabaseRequest
    
    * setting env_var directly in batchScanHandler
    
    * code review

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/batch/BatchScanStartHandler.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/batch/BatchScanStartHandler.java
@@ -4,10 +4,12 @@ import static no.unit.nva.publication.events.handlers.ConfigurationForPushingDir
 import static no.unit.nva.publication.events.handlers.PublicationEventsConfig.objectMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import no.unit.nva.publication.events.bodies.ScanDatabaseRequest;
+import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.ioutils.IoUtils;
 import org.slf4j.Logger;
@@ -24,6 +26,7 @@ public class BatchScanStartHandler implements RequestStreamHandler {
         "Starting scanning with pageSize equal to: %s. Set 'pageSize' between [1,1000] "
         + "if you want a different pageSize value.";
     private static final Logger logger = LoggerFactory.getLogger(BatchScanStartHandler.class);
+    public static final String OUTPUT_EVENT_TOPIC = "OUTPUT_EVENT_TOPIC";
     private final EventBridgeClient client;
 
     @JacocoGenerated
@@ -37,16 +40,21 @@ public class BatchScanStartHandler implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        String inputString = IoUtils.streamToString(input);
-        ScanDatabaseRequest scanRequest =
-            objectMapper.readValue(inputString, ScanDatabaseRequest.class);
+        var inputString = IoUtils.streamToString(input);
+        var scanRequest = createScanDatabaseRequest(inputString);
         logger.info(String.format(INFORMATION_MESSAGE, scanRequest.getPageSize()));
         var event = scanRequest.createNewEventEntry(
             EVENT_BUS_NAME,
             EventBasedBatchScanHandler.DETAIL_TYPE,
             context.getInvokedFunctionArn());
-        PutEventsResponse response = sendEvent(event);
+        var response = sendEvent(event);
         logger.info(response.toString());
+    }
+
+    private static ScanDatabaseRequest createScanDatabaseRequest(String input) throws JsonProcessingException {
+        var request = objectMapper.readValue(input, ScanDatabaseRequest.class);
+        request.setTopic(new Environment().readEnv(OUTPUT_EVENT_TOPIC));
+        return request;
     }
 
     @JacocoGenerated
