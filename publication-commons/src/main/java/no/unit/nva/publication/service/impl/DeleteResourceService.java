e65fc084c6fa86180783ac628bc7e8f6f00aff4d
Kir Truhacev <kir.truhacev@sikt.no>
2023-06-06 13:53:20 +0200

    code review

--- publication-commons/src/main/java/no/unit/nva/publication/service/impl/DeleteResourceService.java
+++ publication-commons/src/main/java/no/unit/nva/publication/service/impl/DeleteResourceService.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.model.Delete;
 import com.amazonaws.services.dynamodbv2.model.TransactWriteItem;
 import com.amazonaws.services.dynamodbv2.model.TransactWriteItemsRequest;
-import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.publication.model.business.ImportCandidate;
 import no.unit.nva.publication.model.business.ImportStatus;
 import no.unit.nva.publication.model.business.Resource;
@@ -15,10 +14,10 @@ import org.slf4j.LoggerFactory;
 
 public class DeleteResourceService extends ServiceWithTransactions {
 
+    private static final Logger logger = LoggerFactory.getLogger(DeleteResourceService.class);
     public static final String CAN_NOT_DELETE_IMPORT_CANDIDATE_MESSAGE = "Can not delete already imported resource!";
     public static final String IMPORT_CANDIDATE_DELETION_MESSAGE = "Import candidate with id ‰s has been permanently "
                                                                    + "deleted {}";
-    private static final Logger logger = LoggerFactory.getLogger(DeleteResourceService.class);
     private final String tableName;
     private final ReadResourceService readResourceService;
 
@@ -31,7 +30,8 @@ public class DeleteResourceService extends ServiceWithTransactions {
     }
 
     public void deleteImportCandidate(ImportCandidate candidate) throws NotFoundException, BadMethodException {
-        var importCandidate = readResourceService.getResourceByIdentifier(candidate.getIdentifier()).toImportCandidate();
+        var importCandidate = readResourceService.getResourceByIdentifier(candidate.getIdentifier())
+                                  .toImportCandidate();
         if (ImportStatus.IMPORTED.equals(importCandidate.getImportStatus())) {
             throw new BadMethodException(CAN_NOT_DELETE_IMPORT_CANDIDATE_MESSAGE);
         } else {
