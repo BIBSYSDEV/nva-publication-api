624dd92be907dfba41d856dd0e15844ec8150a09
Kir Truhacev <kir.truhacev@sikt.no>
2023-06-14 13:29:41 +0200

    update import status handler created

--- publication-rest/src/main/java/no/unit/nva/publication/update/UpdateImportStatusHandler.java
+++ publication-rest/src/main/java/no/unit/nva/publication/update/UpdateImportStatusHandler.java
@@ -1,2 +1,52 @@
-package no.unit.nva.publication.update;public class UpdateImportStatusHandler {
+package no.unit.nva.publication.update;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import java.net.HttpURLConnection;
+import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.publication.exception.NotAuthorizedException;
+import no.unit.nva.publication.model.business.importcandidate.ImportCandidate;
+import no.unit.nva.publication.model.business.importcandidate.ImportStatus;
+import no.unit.nva.publication.service.impl.ResourceService;
+import nva.commons.apigateway.AccessRight;
+import nva.commons.apigateway.ApiGatewayHandler;
+import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
+
+public class UpdateImportStatusHandler extends ApiGatewayHandler<ImportStatus, ImportCandidate> {
+
+    public static final String IMPORT_CANDIDATE_IDENTIFIER_PATH_PARAMETER = "importCandidateIdentifier";
+    private final ResourceService importCandidateService;
+
+    public UpdateImportStatusHandler(ResourceService importCandidateService) {
+        super(ImportStatus.class);
+        this.importCandidateService = importCandidateService;
+    }
+
+    @Override
+    protected ImportCandidate processInput(ImportStatus input, RequestInfo requestInfo, Context context)
+        throws ApiGatewayException {
+        validateAccessRights(requestInfo);
+        var identifier = getIdentifier(requestInfo);
+        return importCandidateService.updateImportStatus(identifier, input);
+    }
+
+    @Override
+    protected Integer getSuccessStatusCode(ImportStatus input, ImportCandidate output) {
+        return HttpURLConnection.HTTP_OK;
+    }
+
+    private SortableIdentifier getIdentifier(RequestInfo requestInfo) {
+        var identifier = requestInfo.getPathParameters().get(IMPORT_CANDIDATE_IDENTIFIER_PATH_PARAMETER);
+        return new SortableIdentifier(identifier);
+    }
+
+    private boolean isNotAuthorized(RequestInfo requestInfo) {
+        return !requestInfo.userIsAuthorized(AccessRight.PROCESS_IMPORT_CANDIDATE.name());
+    }
+
+    private void validateAccessRights(RequestInfo requestInfo) throws NotAuthorizedException {
+        if (isNotAuthorized(requestInfo)) {
+            throw new NotAuthorizedException();
+        }
+    }
 }
--- publication-rest/src/test/java/no/unit/nva/publication/update/UpdateImportStatusHandlerTest.java
+++ publication-rest/src/test/java/no/unit/nva/publication/update/UpdateImportStatusHandlerTest.java
@@ -1,2 +1,157 @@
-package no.unit.nva.publication.update;public class UpdateImportStatusHandlerTest {
+package no.unit.nva.publication.update;
+
+import static no.unit.nva.publication.PublicationRestHandlersTestConfig.restApiMapper;
+import static no.unit.nva.publication.create.CreatePublicationFromImportCandidateHandler.SCOPUS_IDENTIFIER;
+import static no.unit.nva.testutils.RandomDataGenerator.randomDoi;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.model.AdditionalIdentifier;
+import no.unit.nva.model.Contributor;
+import no.unit.nva.model.EntityDescription;
+import no.unit.nva.model.Identity;
+import no.unit.nva.model.Organization;
+import no.unit.nva.model.PublicationDate;
+import no.unit.nva.model.ResearchProject;
+import no.unit.nva.model.ResourceOwner;
+import no.unit.nva.model.Username;
+import no.unit.nva.model.funding.FundingBuilder;
+import no.unit.nva.model.role.Role;
+import no.unit.nva.model.role.RoleType;
+import no.unit.nva.publication.model.business.importcandidate.ImportCandidate;
+import no.unit.nva.publication.model.business.importcandidate.ImportStatus;
+import no.unit.nva.publication.model.business.importcandidate.ImportStatusFactory;
+import no.unit.nva.publication.service.ResourcesLocalTest;
+import no.unit.nva.publication.service.impl.ResourceService;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.apigateway.AccessRight;
+import nva.commons.apigateway.GatewayResponse;
+import nva.commons.apigateway.exceptions.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UpdateImportStatusHandlerTest extends ResourcesLocalTest {
+
+    public static final String IDENTIFIER = "importCandidateIdentifier";
+    public static final String TABLE_NAME = "import-candidates";
+    private static final Context CONTEXT = mock(Context.class);
+    private ByteArrayOutputStream output;
+    private ResourceService importCandidateService;
+    private UpdateImportStatusHandler handler;
+
+    @BeforeEach
+    public void setUp() {
+        super.init(TABLE_NAME);
+        output = new ByteArrayOutputStream();
+        importCandidateService = new ResourceService(client, TABLE_NAME);
+        handler = new UpdateImportStatusHandler(importCandidateService);
+    }
+
+    @Test
+    void shouldReturnUnauthorizedIfUserHasNoAccessRights() throws IOException, NotFoundException {
+        var importCandidate = createPersistedImportCandidate();
+        var request = request(importCandidate, notApplicableImportStatus(), AccessRight.USER);
+        handler.handleRequest(request, output, CONTEXT);
+        var response = GatewayResponse.fromOutputStream(output, ImportCandidate.class);
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_UNAUTHORIZED)));
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenAttemptingToUpdateStatusOnNonExistingImportCandidate() throws IOException {
+        var importCandidate = createImportCandidate();
+        var request = request(importCandidate, notApplicableImportStatus(), AccessRight.PROCESS_IMPORT_CANDIDATE);
+        handler.handleRequest(request, output, CONTEXT);
+        var response = GatewayResponse.fromOutputStream(output, ImportCandidate.class);
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_NOT_FOUND)));
+    }
+
+    @Test
+    void shouldUpdateImportStatusSuccessfully() throws NotFoundException, IOException {
+        var importCandidate = createPersistedImportCandidate();
+        var request = request(importCandidate, notApplicableImportStatus(), AccessRight.PROCESS_IMPORT_CANDIDATE);
+        handler.handleRequest(request, output, CONTEXT);
+        var response = GatewayResponse.fromOutputStream(output, ImportCandidate.class);
+
+        assertThat(response.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_OK)));
+    }
+
+    private static ImportStatus notApplicableImportStatus() {
+        return ImportStatusFactory.createNotApplicable(new Username(randomString()), null);
+    }
+
+    private InputStream request(ImportCandidate importCandidate, ImportStatus importStatus, AccessRight accessRight)
+        throws JsonProcessingException {
+        Map<String, String> pathParameters = Map.of(IDENTIFIER, importCandidate.getIdentifier().toString());
+
+        var customerId = importCandidate.getPublisher().getId();
+        return new HandlerRequestBuilder<ImportStatus>(restApiMapper)
+                   .withUserName(importCandidate.getResourceOwner().getOwner().getValue())
+                   .withCurrentCustomer(customerId)
+                   .withBody(importStatus)
+                   .withAccessRights(customerId, accessRight.name())
+                   .withPathParameters(pathParameters)
+                   .build();
+    }
+
+    private ImportCandidate createPersistedImportCandidate() throws NotFoundException {
+        var candidate = createImportCandidate();
+        var importCandidate = importCandidateService.persistImportCandidate(candidate);
+        return importCandidateService.getImportCandidateByIdentifier(importCandidate.getIdentifier());
+    }
+
+    private ImportCandidate createImportCandidate() {
+        return new ImportCandidate.Builder()
+                   .withImportStatus(ImportStatusFactory.createNotImported())
+                   .withEntityDescription(randomEntityDescription())
+                   .withLink(randomUri())
+                   .withDoi(randomDoi())
+                   .withIndexedDate(Instant.now())
+                   .withPublishedDate(Instant.now())
+                   .withHandle(randomUri())
+                   .withModifiedDate(Instant.now())
+                   .withCreatedDate(Instant.now())
+                   .withPublisher(new Organization.Builder().withId(randomUri()).build())
+                   .withSubjects(List.of(randomUri()))
+                   .withIdentifier(SortableIdentifier.next())
+                   .withRightsHolder(randomString())
+                   .withProjects(List.of(new ResearchProject.Builder().withId(randomUri()).build()))
+                   .withFundings(List.of(new FundingBuilder().build()))
+                   .withAdditionalIdentifiers(Set.of(new AdditionalIdentifier(SCOPUS_IDENTIFIER, randomString())))
+                   .withResourceOwner(new ResourceOwner(new Username(randomString()), randomUri()))
+                   .withAssociatedArtifacts(List.of())
+                   .build();
+    }
+
+    private EntityDescription randomEntityDescription() {
+        return new EntityDescription.Builder()
+                   .withPublicationDate(new PublicationDate.Builder().withYear("2020").build())
+                   .withAbstract(randomString())
+                   .withDescription(randomString())
+                   .withContributors(List.of(randomContributor()))
+                   .withMainTitle(randomString())
+                   .build();
+    }
+
+    private Contributor randomContributor() {
+        return new Contributor.Builder()
+                   .withIdentity(new Identity.Builder().withName(randomString()).build())
+                   .withRole(new RoleType(Role.ACTOR))
+                   .build();
+    }
 }
