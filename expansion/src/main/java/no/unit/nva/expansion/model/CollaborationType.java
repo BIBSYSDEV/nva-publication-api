28a1b6ec96c8ff4b07d74b322a274ba5b0a3c8bf
Kir Truhacev <kir.truhacev@sikt.no>
2023-08-25 13:51:02 +0200

    code review

--- expansion/src/main/java/no/unit/nva/expansion/model/CollaborationType.java
+++ expansion/src/main/java/no/unit/nva/expansion/model/CollaborationType.java
@@ -1,15 +1,18 @@
 package no.unit.nva.expansion.model;
 
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-@Getter
 public enum CollaborationType {
 
     COLLABORATIVE("Collaborative"), NON_COLLABORATIVE("NonCollaborative");
-
     private final String value;
 
     CollaborationType(String value) {
         this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
     }
 }
