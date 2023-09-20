a5261dd4bae463d278b9f8dde9c7395cae7ec36f
Kir Truhacev <kir.truhacev@sikt.no>
2023-09-20 14:53:54 +0200

    topLevelOrganizations to List

--- expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
+++ expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
@@ -84,10 +84,8 @@ import nva.commons.core.paths.UriWrapper;
 import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
