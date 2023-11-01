bf6d64d9940502e3ccaa772a9e91290679db0bba
anetteOlli <anette.olli.siiri@sikt.no>
2023-11-01 13:14:41 +0100

    NP-45017-brage-migration-check-for-duplicates-with-the-same-doi, added test to publication-commons

--- publication-commons/src/main/java/no/unit/nva/publication/model/SearchResourceApiResponse.java
+++ publication-commons/src/main/java/no/unit/nva/publication/model/SearchResourceApiResponse.java
@@ -1,13 +1,13 @@
 package no.unit.nva.publication.model;
 
-import static nva.commons.core.attempt.Try.attempt;
 import java.util.List;
-import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.commons.json.JsonSerializable;
 import no.unit.nva.model.Publication;
 
-public record SearchResourceApiResponse(int totalHits, List<Publication> hits) {
+public record SearchResourceApiResponse(int totalHits, List<Publication> hits) implements JsonSerializable {
+
     @Override
     public String toString() {
-        return attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(this)).orElseThrow();
+        return toJsonString();
     }
 }
