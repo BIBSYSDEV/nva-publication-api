21231a41bc937942077870bc394501a4acc35fb7
anetteOlli <anette.olli.siiri@sikt.no>
2023-11-01 13:33:48 +0100

    NP-45017-brage-migration-check-for-duplicates-with-the-same-doi, jacogenerated on toString method

--- publication-commons/src/main/java/no/unit/nva/publication/model/SearchResourceApiResponse.java
+++ publication-commons/src/main/java/no/unit/nva/publication/model/SearchResourceApiResponse.java
@@ -3,9 +3,11 @@ package no.unit.nva.publication.model;
 import java.util.List;
 import no.unit.nva.commons.json.JsonSerializable;
 import no.unit.nva.model.Publication;
+import nva.commons.core.JacocoGenerated;
 
 public record SearchResourceApiResponse(int totalHits, List<Publication> hits) implements JsonSerializable {
 
+    @JacocoGenerated
     @Override
     public String toString() {
         return toJsonString();
