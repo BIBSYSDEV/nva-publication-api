78570998aefa26895d1972df9b88769d48ca18fc
Eyob Teweldemedhin <eyob.teweldemedhin@sikt.no>
2023-11-13 08:53:03 +0100

    tilpasse koden til den nye datamodellen.

--- brage-import/src/test/java/cucumber/features/transformers/PublishedFileTransformer.java
+++ brage-import/src/test/java/cucumber/features/transformers/PublishedFileTransformer.java
@@ -4,6 +4,10 @@ import io.cucumber.java.DataTableType;
 import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
+import no.unit.nva.model.associatedartifacts.CustomerRightsRetentionStrategy;
+import no.unit.nva.model.associatedartifacts.NullRightsRetentionStrategy;
+import no.unit.nva.model.associatedartifacts.OverriddenRightsRetentionStrategy;
+import no.unit.nva.model.associatedartifacts.RightsRetentionStrategy;
 import no.unit.nva.model.associatedartifacts.file.PublishedFile;
 import org.junit.platform.commons.util.StringUtils;
 
@@ -27,6 +31,7 @@ public class PublishedFileTransformer {
         var license = entry.get("license");
         var publisherAuthority = Boolean.parseBoolean(entry.get("publisherAuthority"));
         var embargoDate = parseDate(entry.get("embargoDate"));
+        var rightsRetentionStrategy = create(entry.get("rightsRetentionStrategy"));
         var publishedDate = parseDate(entry.get("publishedDate"));
         return new PublishedFile(identifier,
                                  fileName,
@@ -36,6 +41,7 @@ public class PublishedFileTransformer {
                                  ADMINISTRATIVE_AGREEMENT_IS_ALWAYS_FALSE_FOR_PUBLISHED_FILES,
                                  publisherAuthority,
                                  embargoDate,
+                                 rightsRetentionStrategy,
                                  publishedDate
         );
     }
@@ -48,5 +54,18 @@ public class PublishedFileTransformer {
 
     private static boolean isNullDate(String candidate) {
         return StringUtils.isBlank(candidate) || "null".equalsIgnoreCase(candidate);
+    }
+
+    public static RightsRetentionStrategy create(String strategyName) {
+        switch (strategyName) {
+            case "CustomerRightsRetentionStrategy":
+                return new CustomerRightsRetentionStrategy();
+            case "OverriddenRightsRetentionStrategy":
+                return new OverriddenRightsRetentionStrategy();
+            case "NullRightsRetentionStrategy":
+                return new NullRightsRetentionStrategy(null);
+            default:
+                throw new IllegalArgumentException("Invalid strategy name: " + strategyName);
+        }
     }
 }
