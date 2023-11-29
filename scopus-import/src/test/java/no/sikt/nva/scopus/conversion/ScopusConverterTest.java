e5ce2bb071ce9f246d02bb58d8770076270c03e1
Kir Truhacev <kir.truhacev@sikt.no>
2023-11-29 10:40:01 +0100

    fixing build

--- scopus-import/src/test/java/no/sikt/nva/scopus/conversion/ScopusConverterTest.java
+++ scopus-import/src/test/java/no/sikt/nva/scopus/conversion/ScopusConverterTest.java
@@ -3,7 +3,9 @@ package no.sikt.nva.scopus.conversion;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import no.sikt.nva.scopus.ScopusConverter;
 import no.sikt.nva.scopus.conversion.files.ScopusFileConverter;
 import no.sikt.nva.scopus.utils.ScopusGenerator;
@@ -15,9 +17,12 @@ public class ScopusConverterTest {
     void shouldReturnImportCandidateWithoutTitleWhenTitleListFromScopusXmlISEmpty() {
         var generator = new ScopusGenerator();
         generator.getDocument().getItem().getItem().getBibrecord().getHead().getCitationTitle().getTitletext().clear();
+        var customerConnection = mock(NvaCustomerConnection.class);
         var converter = new ScopusConverter(generator.getDocument(), mock(PiaConnection.class),
                                             mock(CristinConnection.class), mock(PublicationChannelConnection.class),
+                                            customerConnection,
                                             mock(ScopusFileConverter.class));
+        when(customerConnection.isNvaCustomer(any())).thenReturn(true);
         var candidate = converter.generateImportCandidate();
 
         assertThat(candidate.getEntityDescription().getMainTitle(), is(nullValue()));
@@ -27,9 +32,12 @@ public class ScopusConverterTest {
     void shouldReturnImportCandidateWithoutTitleWhenTitleFromXmlIsNull() {
         var generator = new ScopusGenerator();
         setNullTitle(generator);
+        var customerConnection = mock(NvaCustomerConnection.class);
         var converter = new ScopusConverter(generator.getDocument(), mock(PiaConnection.class),
                                             mock(CristinConnection.class), mock(PublicationChannelConnection.class),
+                                            customerConnection,
                                             mock(ScopusFileConverter.class));
+        when(customerConnection.isNvaCustomer(any())).thenReturn(true);
         var candidate = converter.generateImportCandidate();
 
         assertThat(candidate.getEntityDescription().getMainTitle(), is(nullValue()));
