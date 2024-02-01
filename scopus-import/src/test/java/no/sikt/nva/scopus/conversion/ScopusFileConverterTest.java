f2932d6d666b86851270b7faa4bccc58b032c615
Kir Truhacev <kir.truhacev@sikt.no>
2024-02-01 12:29:30 +0100

    code clean up

--- scopus-import/src/test/java/no/sikt/nva/scopus/conversion/ScopusFileConverterTest.java
+++ scopus-import/src/test/java/no/sikt/nva/scopus/conversion/ScopusFileConverterTest.java
@@ -157,16 +157,6 @@ public class ScopusFileConverterTest {
         assertThat(files, is(emptyIterable()));
     }
 
-//    @Test
-//    void shouldExtractFileMimeTypeFromDownloadFileUrlResponseHeaderWhenCrossRefResponseMissesContentType()
-//        throws IOException, InterruptedException {
-//        mockResponsesWithoutHeaders("crossrefResponseMissingFields.json");
-//        mockDownloadUrlResponse();
-//        var file = (PublishedFile) fileConverter.fetchAssociatedArtifacts(scopusData.getDocument()).get(0);
-//
-//        assertThat(file.getMimeType(), is(equalTo(HEADER_CONTENT_TYPE)));
-//    }
-
     @Test
     void shouldCreateRandomFileNameWithFileTypeFromContentTypeHeaderWhenUrlAndContentDispositionMissingFileName()
         throws IOException, InterruptedException {
