f86f8a054fc9fb80a83d1f949e759511fe86590d
anetteOlli <anette.olli.siiri@sikt.no>
2024-03-12 10:23:32 +0100

    fixing-rights-retention-strategy-bug, wip

--- tickets-test/src/main/java/no/unit/nva/publication/ticket/test/TicketTestUtils.java
+++ tickets-test/src/main/java/no/unit/nva/publication/ticket/test/TicketTestUtils.java
@@ -330,6 +330,6 @@ public final class TicketTestUtils {
     private static AdministrativeAgreement administrativeAgreement() {
         var license = new License.Builder().withLink(randomUri()).withIdentifier("identifier").build();
         return new AdministrativeAgreement(UUID.randomUUID(), "name", "application/json",
-                                           123124124L, license, true, false, null, null);
+                                           123124124L, license, true, false, null);
     }
 }
