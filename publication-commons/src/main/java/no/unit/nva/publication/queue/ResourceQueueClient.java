af3cf08280fe93adcd897aa54d5761c21d33834d
Kir Truhacev <kir.truhacev@sikt.no>
2024-03-22 16:44:06 +0100

    implementing recovery batch scan

--- publication-commons/src/main/java/no/unit/nva/publication/queue/ResourceQueueClient.java
+++ publication-commons/src/main/java/no/unit/nva/publication/queue/ResourceQueueClient.java
@@ -1,40 +1,67 @@
 package no.unit.nva.publication.queue;
 
 import java.time.Duration;
+import java.util.List;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @JacocoGenerated
-public final class ResourceQueueClient implements QueueClient{
+public final class ResourceQueueClient implements QueueClient {
 
+    public static final int MAXIMUM_NUMBER_OF_MESSAGES = 10;
     public static final String AWS_REGION = "AWS_REGION";
     private static final int MAX_CONNECTIONS = 10_000;
     private static final long IDLE_TIME = 30;
     private static final long TIMEOUT_TIME = 30;
     private final SqsClient sqsClient;
 
-    public static ResourceQueueClient defaultResourceQueueClient() {
-        return new ResourceQueueClient(defaultClient());
-    }
-
     private ResourceQueueClient(SqsClient sqsClient) {
         this.sqsClient = sqsClient;
     }
 
-    private static SqsClient defaultClient() {
-        return SqsClient.builder()
-                   .region(getRegion())
-                   .httpClient(httpClientForConcurrentQueries())
-                   .build();
+    public static ResourceQueueClient defaultResourceQueueClient() {
+        return new ResourceQueueClient(defaultClient());
     }
+
     @Override
     public void sendMessage(SendMessageRequest sendMessageRequest) {
         sqsClient.sendMessage(sendMessageRequest);
+    }
+
+    @Override
+    public List<Message> readMessages(String queue) {
+        var receiveMessageRequest = ReceiveMessageRequest.builder()
+                                        .queueUrl(queue)
+                                        .maxNumberOfMessages(MAXIMUM_NUMBER_OF_MESSAGES)
+                                        .build();
+        return sqsClient.receiveMessage(receiveMessageRequest).messages();
+    }
+
+    @Override
+    public void deleteMessages(String queue, List<Message> messages) {
+        var entriesToDelete = messages.stream().map(ResourceQueueClient::toDeleteMessageRequest).toList();
+        var deleteMessagesBatchRequest = DeleteMessageBatchRequest.builder()
+                                             .queueUrl(queue)
+                                             .entries(entriesToDelete)
+                                             .build();
+        sqsClient.deleteMessageBatch(deleteMessagesBatchRequest);
+    }
+
+    private static SqsClient defaultClient() {
+        return SqsClient.builder().region(getRegion()).httpClient(httpClientForConcurrentQueries()).build();
+    }
+
+    private static DeleteMessageBatchRequestEntry toDeleteMessageRequest(Message message) {
+        return DeleteMessageBatchRequestEntry.builder().id(message.messageId()).build();
     }
 
     private static Region getRegion() {
---
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/recovery/RecoveryBatchScanHandler.java
@@ -0,0 +1,71 @@
+package no.unit.nva.publication.events.handlers.recovery;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import java.util.List;
+import no.unit.nva.events.handlers.EventHandler;
+import no.unit.nva.events.models.AwsEventBridgeEvent;
+import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.publication.queue.QueueClient;
+import no.unit.nva.publication.queue.ResourceQueueClient;
+import no.unit.nva.publication.service.impl.ResourceService;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+
+public class RecoveryBatchScanHandler extends EventHandler<RecoveryEventRequest, Void> {
+
+    public static final String RECOVERY_QUEUE = new Environment().readEnv("RECOVERY_QUEUE");
+    public static final String ID = "id";
+    public static final int MAX_NUMBER_OF_MESSAGES_PER_RECOVERY_REQUEST = 10;
+    private final EventBridgeClient eventBridgeClient;
+    private final QueueClient queueClient;
+    private final ResourceService resourceService;
+
+    @JacocoGenerated
+    public RecoveryBatchScanHandler() {
+        this(ResourceService.defaultService(), ResourceQueueClient.defaultResourceQueueClient(),
+             EventBridgeClient.create());
+    }
+
+    public RecoveryBatchScanHandler(ResourceService resourceService, QueueClient queueClient,
+                                    EventBridgeClient eventBridgeClient) {
+        super(RecoveryEventRequest.class);
+        this.resourceService = resourceService;
+        this.queueClient = queueClient;
+        this.eventBridgeClient = eventBridgeClient;
+    }
+
+    @Override
+    protected Void processInput(RecoveryEventRequest recoveryRequest,
+                                AwsEventBridgeEvent<RecoveryEventRequest> awsEventBridgeEvent, Context context) {
+
+        var messages = queueClient.readMessages(RECOVERY_QUEUE);
+
+        messages.stream()
+            .map(RecoveryBatchScanHandler::extractResourceIdentifier)
+            .forEach(resourceService::refresh);
+
+        queueClient.deleteMessages(RECOVERY_QUEUE, messages);
+
+        if (moreMessagesOnQueue(messages)) {
+            emitNewRecoveryEvent();
+        }
+
+        return null;
+    }
+
+    private static boolean moreMessagesOnQueue(List<Message> messages) {
+        return messages.size() == MAX_NUMBER_OF_MESSAGES_PER_RECOVERY_REQUEST;
+    }
+
+    private static SortableIdentifier extractResourceIdentifier(Message message) {
+        return new SortableIdentifier(message.messageAttributes().get(ID).stringValue());
+    }
+
+    private void emitNewRecoveryEvent() {
+        var eventEntry = RecoveryEventRequest.createNewEntry();
+        eventBridgeClient.putEvents(PutEventsRequest.builder().entries(eventEntry).build());
+    }
+}
