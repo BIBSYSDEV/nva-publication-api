fcda954629b2d89b87e7f021031092df03e2f71e
Kir Truhacev <kir.truhacev@sikt.no>
2024-03-25 08:32:43 +0100

    logging messages

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/recovery/RecoveryBatchScanHandler.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/recovery/RecoveryBatchScanHandler.java
@@ -5,17 +5,21 @@ import java.util.List;
 import no.unit.nva.events.handlers.EventHandler;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
 import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.publication.events.handlers.expandresources.ExpandDataEntriesHandler;
 import no.unit.nva.publication.queue.QueueClient;
 import no.unit.nva.publication.queue.ResourceQueueClient;
 import no.unit.nva.publication.service.impl.ResourceService;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 import software.amazon.awssdk.services.sqs.model.Message;
 
 public class RecoveryBatchScanHandler extends EventHandler<RecoveryEventRequest, Void> {
 
+    private static final Logger logger = LoggerFactory.getLogger(RecoveryBatchScanHandler.class);
     public static final String RECOVERY_QUEUE = new Environment().readEnv("RECOVERY_QUEUE");
     public static final String ID = "id";
     public static final int MAX_NUMBER_OF_MESSAGES_PER_RECOVERY_REQUEST = 10;
@@ -42,7 +46,8 @@ public class RecoveryBatchScanHandler extends EventHandler<RecoveryEventRequest,
                                 AwsEventBridgeEvent<RecoveryEventRequest> awsEventBridgeEvent, Context context) {
 
         var messages = queueClient.readMessages();
-
+        logger.info("Number of extracted messages: {}", messages.size());
+        logger.info("Number of extracted messages: {}", messages.getFirst());
         messages.stream()
             .map(RecoveryBatchScanHandler::extractResourceIdentifier)
             .forEach(resourceService::refresh);
