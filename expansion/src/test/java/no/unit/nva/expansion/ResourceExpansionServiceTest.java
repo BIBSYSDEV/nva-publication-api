f8ef5869436312e25514c55bb677cd223bd49355
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-04-22 14:03:08 +0200

    adding new license (#1626)

--- expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
+++ expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
@@ -734,6 +734,7 @@ class ResourceExpansionServiceTest extends ResourcesLocalTest {
                        Arguments.of("https://creativecommons.org/licenses/by-sa/4.0", LicenseType.CC_SA),
                        Arguments.of("https://creativecommons.org/licenses/by/4.0", LicenseType.CC_BY),
                        Arguments.of("https://creativecommons.org/publicdomain/zero/1.0/", LicenseType.CC_ZERO),
+                       Arguments.of("http://rightsstatements.org/vocab/InC/1.0/", LicenseType.RS_INC),
                        Arguments.of("https://something.else.com", LicenseType.OTHER)
         );
     }
