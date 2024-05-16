df08500f89fa0fff43cfc64ec48a638285eb3096
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-05-16 13:48:30 +0200

    merge book and degree data (#1659)
    * merge book and degree data
    
    * merging degree phd

--- brage-import/src/main/java/no/sikt/nva/brage/migration/merger/CristinImportPublicationMerger.java
+++ brage-import/src/main/java/no/sikt/nva/brage/migration/merger/CristinImportPublicationMerger.java
@@ -11,13 +11,27 @@ import no.unit.nva.model.AdditionalIdentifier;
 import no.unit.nva.model.Course;
 import no.unit.nva.model.EntityDescription;
 import no.unit.nva.model.Publication;
+import no.unit.nva.model.PublicationDate;
 import no.unit.nva.model.Reference;
 import no.unit.nva.model.associatedartifacts.AssociatedArtifactList;
+import no.unit.nva.model.contexttypes.Book;
+import no.unit.nva.model.contexttypes.Book.BookBuilder;
+import no.unit.nva.model.contexttypes.BookSeries;
 import no.unit.nva.model.contexttypes.Degree;
 import no.unit.nva.model.contexttypes.Degree.Builder;
 import no.unit.nva.model.contexttypes.PublicationContext;
+import no.unit.nva.model.contexttypes.Publisher;
+import no.unit.nva.model.contexttypes.PublishingHouse;
+import no.unit.nva.model.contexttypes.Series;
+import no.unit.nva.model.contexttypes.UnconfirmedPublisher;
+import no.unit.nva.model.contexttypes.UnconfirmedSeries;
 import no.unit.nva.model.exceptions.InvalidIsbnException;
 import no.unit.nva.model.exceptions.InvalidUnconfirmedSeriesException;
+import no.unit.nva.model.instancetypes.PublicationInstance;
+import no.unit.nva.model.instancetypes.degree.DegreePhd;
+import no.unit.nva.model.instancetypes.degree.RelatedDocument;
+import no.unit.nva.model.pages.MonographPages;
+import no.unit.nva.model.pages.Pages;
 import nva.commons.core.StringUtils;
 
 public class CristinImportPublicationMerger {
@@ -28,8 +42,8 @@ public class CristinImportPublicationMerger {
     private final Publication cristinPublication;
     private final Publication bragePublication;
 
-    public CristinImportPublicationMerger(Publication cristinPublication, Publication bragePublication) {
-        this.cristinPublication = cristinPublication;
+    public CristinImportPublicationMerger(Publication existingPublication, Publication bragePublication) {
+        this.cristinPublication = existingPublication;
         this.bragePublication = bragePublication;
     }
 
@@ -54,8 +68,42 @@ public class CristinImportPublicationMerger {
     private Reference determineReference() throws InvalidIsbnException, InvalidUnconfirmedSeriesException {
         var reference = cristinPublication.getEntityDescription().getReference();
         reference.setPublicationContext(determinePublicationContext(reference));
+        reference.setPublicationInstance(determincePublicationInstance(reference));
         reference.setDoi(determineDoi(reference));
         return reference;
+    }
+
+    private PublicationInstance<? extends Pages> determincePublicationInstance(Reference reference) {
+        var publicationInstance = reference.getPublicationInstance();
+        var bragePublicationInstance = bragePublication.getEntityDescription().getReference().getPublicationInstance();
+
+        if (publicationInstance instanceof DegreePhd degreePhd && bragePublicationInstance instanceof DegreePhd brageDegreePhd) {
+            return new DegreePhd(getPages(degreePhd.getPages(), brageDegreePhd.getPages()),
+                                 getDate(degreePhd.getSubmittedDate(), brageDegreePhd.getSubmittedDate()),
+                                 getRelated(degreePhd.getRelated(), brageDegreePhd.getRelated()));
+        }
+        else {
+            return publicationInstance;
+        }
+    }
+
+    private Set<RelatedDocument> getRelated(Set<RelatedDocument> documents, Set<RelatedDocument> brageDocuments) {
+        if (nonNull(documents)) {
+            var mergedDocuments = new HashSet<RelatedDocument>();
+            mergedDocuments.addAll(documents);
+            mergedDocuments.addAll(brageDocuments);
+            return mergedDocuments;
+        } else {
+            return brageDocuments;
+        }
+    }
+
+    private PublicationDate getDate(PublicationDate submittedDate, PublicationDate brageDate) {
+        return nonNull(submittedDate) ? submittedDate : brageDate;
+    }
+
+    private MonographPages getPages(MonographPages pages, MonographPages bragePages) {
+        return nonNull(pages) ? pages : bragePages;
     }
 
     private URI determineDoi(Reference reference) {
@@ -68,20 +116,68 @@ public class CristinImportPublicationMerger {
 
     private PublicationContext determinePublicationContext(Reference reference) throws InvalidIsbnException, InvalidUnconfirmedSeriesException {
         var publicationContext = reference.getPublicationContext();
-        if (publicationContext instanceof Degree degree) {
-            return new Builder().withIsbnList(degree.getIsbnList())
-                               .withSeries(degree.getSeries())
-                               .withPublisher(degree.getPublisher())
-                               .withSeriesNumber(degree.getSeriesNumber())
-                               .withCourse(determineCourse(degree))
+        var bragePublicationContext = bragePublication.getEntityDescription().getReference().getPublicationContext();
+        if (publicationContext instanceof Degree degree && bragePublicationContext instanceof Degree brageDegree) {
+            return new Builder().withIsbnList(getIsbnList(degree.getIsbnList(), brageDegree.getIsbnList()))
+                               .withSeries(getSeries(degree.getSeries(), brageDegree.getSeries()))
+                               .withPublisher(getPublisher(degree.getPublisher(), brageDegree.getPublisher()))
+                               .withSeriesNumber(getSeriesNumber(degree, brageDegree))
+                               .withCourse(getCourse(degree))
                                .build();
-        } else {
+        }
+        if (publicationContext instanceof Book book && bragePublicationContext instanceof Book brageBook) {
+            return new BookBuilder()
+                       .withIsbnList(getIsbnList(book.getIsbnList(), brageBook.getIsbnList()))
+                       .withPublisher(getPublisher(book.getPublisher(), brageBook.getPublisher()))
+                       .withSeries(getSeries(book.getSeries(), brageBook.getSeries()))
+                       .withSeriesNumber(getSeriesNumber(book, brageBook))
+                       .withRevision(nonNull(book.getRevision()) ? book.getRevision() : brageBook.getRevision())
+                       .build();
+        }
+        else {
             return publicationContext;
         }
     }
 
-    private Course determineCourse(Degree degree) {
+    private Course getCourse(Degree degree) {
         return nonNull(degree.getCourse()) ? degree.getCourse() : extractBrageCourse();
+    }
+
+    private static String getSeriesNumber(Book book, Book brageBook) {
+        return nonNull(book.getSeriesNumber()) ? book.getSeriesNumber() :
+                                                                                brageBook.getSeriesNumber();
+    }
+
+    private PublishingHouse getPublisher(PublishingHouse existingPublisher, PublishingHouse bragePublisher) {
+        if (nonNull(existingPublisher) && existingPublisher instanceof Publisher publisher) {
+            return publisher;
+        }
+        if (nonNull(bragePublisher) && bragePublisher instanceof Publisher publisher) {
+            return publisher;
+        }
+        if (nonNull(existingPublisher) && existingPublisher instanceof UnconfirmedPublisher unconfirmedPublisher) {
+            return unconfirmedPublisher;
+        } else {
+            return bragePublisher;
+        }
+    }
+
+    private static BookSeries getSeries(BookSeries degree, BookSeries brageDegree) {
+        if (nonNull(degree) && degree instanceof Series series) {
+            return series;
+        }
+        if (nonNull(brageDegree) && brageDegree instanceof Series series) {
+            return series;
+        }
+        if (nonNull(degree) && degree instanceof UnconfirmedSeries unconfirmedSeries) {
+            return unconfirmedSeries;
+        } else {
+            return brageDegree;
+        }
+    }
+
+    private static List<String> getIsbnList(List<String> existingList, List<String> brageList) {
+        return nonNull(existingList) && !existingList.isEmpty() ? existingList : brageList;
     }
 
     private Course extractBrageCourse() {
---
+++ brage-import/src/test/java/no/sikt/nva/brage/migration/merger/CristinImportPublicationMergerTest.java
@@ -0,0 +1,68 @@
+package no.sikt.nva.brage.migration.merger;
+
+import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValues;
+import static no.unit.nva.hamcrest.DoesNotHaveEmptyValues.doesNotHaveEmptyValuesIgnoringFields;
+import static no.unit.nva.model.testing.PublicationGenerator.randomPublication;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.Set;
+import no.unit.nva.model.Publication;
+import no.unit.nva.model.contexttypes.Book;
+import no.unit.nva.model.exceptions.InvalidIsbnException;
+import no.unit.nva.model.exceptions.InvalidUnconfirmedSeriesException;
+import no.unit.nva.model.instancetypes.book.BookAnthology;
+import no.unit.nva.model.instancetypes.degree.DegreePhd;
+import org.junit.jupiter.api.Test;
+
+class CristinImportPublicationMergerTest {
+
+    @Test
+    void shouldUseBrageBookWhenExistingBookIsEmpty() throws InvalidUnconfirmedSeriesException, InvalidIsbnException {
+        var existingPublication = randomPublication(BookAnthology.class);
+        existingPublication.getEntityDescription().getReference().setPublicationContext(emptyBook());
+        var bragePublication = randomPublication(BookAnthology.class);
+
+        var updatedPublication = mergePublications(existingPublication, bragePublication);
+
+        assertThat(updatedPublication.getEntityDescription().getReference().getPublicationContext(),
+                   doesNotHaveEmptyValuesIgnoringFields(Set.of("additionalIdentifiers")));
+    }
+
+    @Test
+    void shouldUseExistingBookWhenBrageBookIsEmpty() throws InvalidUnconfirmedSeriesException, InvalidIsbnException {
+        var bragePublication = randomPublication(BookAnthology.class);
+        bragePublication.getEntityDescription().getReference().setPublicationContext(emptyBook());
+        var existingPublication = randomPublication(BookAnthology.class);
+
+        var updatedPublication = mergePublications(existingPublication, bragePublication);
+
+        assertThat(updatedPublication.getEntityDescription().getReference().getPublicationContext(),
+                   doesNotHaveEmptyValuesIgnoringFields(Set.of("additionalIdentifiers")));
+    }
+
+    @Test
+    void shouldFillExistingEmptyDegreePhdWithBrageDegreePhd() throws InvalidUnconfirmedSeriesException,
+                                                                InvalidIsbnException {
+        var existingPublication = randomPublication(DegreePhd.class);
+        existingPublication.getEntityDescription().getReference().setPublicationInstance(emptyDegreePhd());
+        var bragePublication = randomPublication(DegreePhd.class);
+
+        var updatedPublication = mergePublications(bragePublication, bragePublication);
+
+        assertThat(updatedPublication.getEntityDescription().getReference().getPublicationInstance(),
+                   doesNotHaveEmptyValues());
+    }
+
+    private static Publication mergePublications(Publication existingPublication, Publication bragePublication)
+        throws InvalidIsbnException, InvalidUnconfirmedSeriesException {
+        return new CristinImportPublicationMerger(existingPublication,
+                                                  bragePublication).mergePublications();
+    }
+
+    private static Book emptyBook() throws InvalidUnconfirmedSeriesException {
+        return new Book(null, null, null, null, null, null);
+    }
+
+    private static DegreePhd emptyDegreePhd() {
+        return new DegreePhd(null, null, null);
+    }
+}
