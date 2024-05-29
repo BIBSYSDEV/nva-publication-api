e0af90047d25fdfbd98f5ee7b0eca4abfaddd4bc
anetteOlli <anette.olli.siiri@sikt.no>
2024-05-29 12:02:18 +0200

    NP-46803-expand-channel-registry-journal, code-review

--- expansion/src/main/java/no/unit/nva/expansion/model/ExpandedJournal.java
+++ expansion/src/main/java/no/unit/nva/expansion/model/ExpandedJournal.java
@@ -4,54 +4,10 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.net.URI;
-import java.util.Objects;
-import nva.commons.core.JacocoGenerated;
 
 @JsonTypeInfo(use = Id.NAME, property = "type")
 @JsonTypeName(ExpandedJournal.TYPE)
-public class ExpandedJournal {
+public record ExpandedJournal(URI id, String name) {
 
     public static final String TYPE = "Journal";
-    public URI id;
-
-    public String name;
-
-    @JacocoGenerated
-    public ExpandedJournal() {
-
-    }
-
-    public ExpandedJournal(URI id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @JacocoGenerated
-    public URI getId() {
-        return id;
-    }
-
-    @JacocoGenerated
-    public String getName() {
-        return name;
-    }
-
-    @JacocoGenerated
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @JacocoGenerated
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ExpandedJournal that = (ExpandedJournal) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
-    }
 }
