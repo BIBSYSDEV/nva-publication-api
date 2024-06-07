7e6b28c94bfdc32bc5a3cb803683dd9d9de34d90
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2024-06-07 09:49:20 +0200

    Np 46910 brage merging should lookup several places (#1693)
    * NP-46910-brage-merging-should-lookup-several-places, implemented OR check
    
    * NP-46910-brage-merging-should-lookup-several-places, refactor
    
    * NP-46910-brage-merging-should-lookup-several-places, codacy
    
    * NP-46910-brage-merging-should-lookup-several-places, codacy
    
    * NP-46910-brage-merging-should-lookup-several-places, codacy

--- brage-import/src/main/java/no/sikt/nva/brage/migration/merger/DuplicatePublicationException.java
+++ brage-import/src/main/java/no/sikt/nva/brage/migration/merger/DuplicatePublicationException.java
@@ -2,7 +2,7 @@ package no.sikt.nva.brage.migration.merger;
 
 public class DuplicatePublicationException extends RuntimeException {
 
-    public DuplicatePublicationException(String message, Exception exception) {
-        super(message, exception);
+    public DuplicatePublicationException(String message) {
+        super(message);
     }
 }
---
+++ brage-import/src/main/java/no/sikt/nva/brage/migration/merger/findexistingpublication/FindExistingPublicationServiceImpl.java
@@ -0,0 +1,42 @@
+package no.sikt.nva.brage.migration.merger.findexistingpublication;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import no.sikt.nva.brage.migration.model.PublicationForUpdate;
+import no.sikt.nva.brage.migration.model.PublicationRepresentation;
+import no.unit.nva.publication.external.services.UriRetriever;
+import no.unit.nva.publication.service.impl.ResourceService;
+
+public class FindExistingPublicationServiceImpl implements FindExistingPublicationService {
+
+    private final ResourceService resourceService;
+    private final UriRetriever uriRetriever;
+    private final String apiHost;
+
+    public FindExistingPublicationServiceImpl(ResourceService resourceService, UriRetriever uriRetriever, String apiHost) {
+        this.resourceService = resourceService;
+        this.uriRetriever = uriRetriever;
+        this.apiHost = apiHost;
+    }
+
+    @Override
+    public Optional<PublicationForUpdate> findExistingPublication(PublicationRepresentation publicationRepresentation) {
+        var cristinIdentifierFinder = new CristinIdentifierFinder(resourceService);
+        var doiFinder = new DoiPublicationFinder(resourceService, uriRetriever, apiHost);
+        var isbnFinder = new IsbnPublicationFinder(resourceService, uriRetriever, apiHost);
+        var titleAndTypeFinder = new TitleAndTypePublicationFinder(resourceService, uriRetriever, apiHost);
+
+        List<Supplier<Optional<PublicationForUpdate>>> updatePublicationSuppliers = List.of(
+            () -> cristinIdentifierFinder.findExistingPublication(publicationRepresentation),
+            () -> doiFinder.findExistingPublication(publicationRepresentation),
+            () -> isbnFinder.findExistingPublication(publicationRepresentation),
+            () -> titleAndTypeFinder.findExistingPublication(publicationRepresentation));
+
+        return updatePublicationSuppliers.stream()
+                   .map(Supplier::get)
+                   .filter(Optional::isPresent)
+                   .findFirst()
+                   .orElse(Optional.empty());
+    }
+}
