95c85206d1141b361c5db389d3e58161ab2137da
anetteOlli <anette.olli.siiri@sikt.no>
2024-06-13 15:21:27 +0200

    NP-47010-resource-service-assume-eventual-consistency-will-happen, removed unused classes and constants

--- publication-commons/src/test/java/no/unit/nva/publication/exception/ExceptionsTest.java
+++ publication-commons/src/test/java/no/unit/nva/publication/exception/ExceptionsTest.java
@@ -10,24 +10,7 @@ import org.junit.jupiter.api.Test;
 class ExceptionsTest {
     
     public static final String MESSAGE = "Message";
-    
-    @Test
-    void dynamoDBExceptionHasStatusCode() {
-        ApiGatewayException exception = new DynamoDBException(MESSAGE, new RuntimeException());
-        Assertions.assertEquals(HttpStatus.SC_BAD_GATEWAY, exception.getStatusCode());
-    }
-    
-    @Test
-    void errorResponseExceptionHasStatusCode() {
-        ApiGatewayException exception = new ErrorResponseException(MESSAGE);
-        Assertions.assertEquals(HttpStatus.SC_BAD_GATEWAY, exception.getStatusCode());
-    }
-    
-    @Test
-    void noResponseExceptionHasStatusCode() {
-        ApiGatewayException exception = new NoResponseException(MESSAGE, new RuntimeException());
-        Assertions.assertEquals(HttpStatus.SC_SERVICE_UNAVAILABLE, exception.getStatusCode());
-    }
+
     
     @Test
     void inputExceptionHasStatusCode() {
@@ -40,10 +23,5 @@ class ExceptionsTest {
         ApiGatewayException exception = new NotFoundException(MESSAGE);
         Assertions.assertEquals(HttpStatus.SC_NOT_FOUND, exception.getStatusCode());
     }
-    
-    @Test
-    void notImplementedExceptionHasStatusCode() {
-        ApiGatewayException exception = new NotImplementedException();
-        Assertions.assertEquals(HttpStatus.SC_NOT_IMPLEMENTED, exception.getStatusCode());
-    }
+
 }
