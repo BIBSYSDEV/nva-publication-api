9f6b50d3c0a59eebbcbe4250165698c6781855d1
Kir Truhacev <kir.truhacev@sikt.no>
2024-08-29 10:12:05 +0200

    fixing codacy

--- cristin-import/src/main/java/no/unit/nva/cristin/mapper/nva/exceptions/AffiliationWithoutRoleException.java
+++ cristin-import/src/main/java/no/unit/nva/cristin/mapper/nva/exceptions/AffiliationWithoutRoleException.java
@@ -3,6 +3,7 @@ package no.unit.nva.cristin.mapper.nva.exceptions;
 public final class AffiliationWithoutRoleException extends RuntimeException {
 
     private AffiliationWithoutRoleException() {
+        super();
     }
 
     public static String name() {
