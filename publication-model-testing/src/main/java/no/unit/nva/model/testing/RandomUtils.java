af4ee3d9e89b23f34f0b8ed60622d2a8ac9c665d
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2024-09-02 14:44:24 +0200

    refactor: Add "resource" prefix to mocked test URIs

--- publication-model-testing/src/main/java/no/unit/nva/model/testing/RandomUtils.java
+++ publication-model-testing/src/main/java/no/unit/nva/model/testing/RandomUtils.java
@@ -2,15 +2,20 @@ package no.unit.nva.model.testing;
 
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
-import java.net.URI;
-import java.util.Map;
+
 import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.model.PublicationDate;
+
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UriWrapper;
 
+import java.net.URI;
+import java.util.Map;
+
 @JacocoGenerated
 public class RandomUtils {
+
+    private static final String PUBLICATION_ENDPOINT_NAME = "publication";
 
     public static Map<String, String> randomLabels() {
         return Map.of(RandomLanguageUtil.randomBcp47CompatibleLanguage(), randomString());
@@ -21,7 +26,11 @@ public class RandomUtils {
     }
 
     public static URI randomPublicationId() {
-        return UriWrapper.fromUri(randomUri()).addChild(SortableIdentifier.next().toString()).getUri();
+        var publicationId = SortableIdentifier.next().toString();
+        return UriWrapper.fromUri(randomUri())
+                .addChild(PUBLICATION_ENDPOINT_NAME)
+                .addChild(publicationId)
+                .getUri();
     }
 
     public static PublicationDate randomPublicationDate() {
