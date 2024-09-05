885e11128d94f8cf743ab2fe15f365c89b050b9f
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-09-05 08:29:46 +0200

    Np set institution short name as archive (#1809)
    * setting institution shortName as archive in ImportDetails
    
    * setting institution shortName as archive in ImportDetails

--- brage-import/src/test/java/no/sikt/nva/brage/migration/mapper/BrageNvaMapperTest.java
+++ brage-import/src/test/java/no/sikt/nva/brage/migration/mapper/BrageNvaMapperTest.java
@@ -1,6 +1,7 @@
 package no.sikt.nva.brage.migration.mapper;
 
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anEmptyMap;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,19 +13,26 @@ import java.util.Set;
 import java.util.UUID;
 import no.sikt.nva.brage.migration.NvaType;
 import no.sikt.nva.brage.migration.record.Contributor;
+import no.sikt.nva.brage.migration.record.Customer;
 import no.sikt.nva.brage.migration.record.Identity;
 import no.sikt.nva.brage.migration.record.Type;
 import no.sikt.nva.brage.migration.record.content.ContentFile;
 import no.sikt.nva.brage.migration.record.content.ResourceContent;
 import no.sikt.nva.brage.migration.record.content.ResourceContent.BundleType;
+import no.sikt.nva.brage.migration.record.license.License;
+import no.sikt.nva.brage.migration.record.license.NvaLicense;
 import no.sikt.nva.brage.migration.testutils.NvaBrageMigrationDataGenerator;
+import no.unit.nva.model.Publication;
 import no.unit.nva.model.associatedartifacts.file.AdministrativeAgreement;
+import no.unit.nva.model.associatedartifacts.file.File;
+import no.unit.nva.model.associatedartifacts.file.ImportUploadDetails;
 import no.unit.nva.model.exceptions.InvalidIsbnException;
 import no.unit.nva.model.exceptions.InvalidIssnException;
 import no.unit.nva.model.exceptions.InvalidUnconfirmedSeriesException;
 import no.unit.nva.model.instancetypes.degree.DegreePhd;
 import no.unit.nva.model.instancetypes.degree.UnconfirmedDocument;
 import nva.commons.core.Environment;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 class BrageNvaMapperTest {
@@ -33,6 +41,7 @@ class BrageNvaMapperTest {
     public static final String RELATED_DOCUMENT_3 = "Paper 3: Prestmo A";
     public static final String RELATED_DOCUMENT_1 = "Paper 1: Saltvedt,";
     public static final String RELATED_DOCUMENT_2 = "Paper 2: Prestmo, ";
+    public static final String NPOLAR_SHORT_NAME = "NPI";
 
     @Test
     void shouldMapContentFileWithBundleTypeLicenseToAdministrativeAgreement()
@@ -131,6 +140,28 @@ class BrageNvaMapperTest {
         assertEquals(contributor.getIdentity().getOrcId(), actualContributor.getIdentity().getOrcId());
     }
 
+    @Test
+    void shouldImportFileFromBrageWithImportUploadDetailWithArchiveAsInstitutionShortName()
+        throws InvalidIssnException, InvalidIsbnException, InvalidUnconfirmedSeriesException {
+        var generator =  new NvaBrageMigrationDataGenerator.Builder()
+                             .withType(new Type(List.of(), NvaType.DOCTORAL_THESIS.getValue()))
+                             .withCustomer(new Customer("npolar", randomUri()))
+                             .withResourceContent(new ResourceContent(List.of(createRandomContentFileWithBundleType(BundleType.ORIGINAL))))
+                             .build();
+        var publication = BrageNvaMapper.toNvaPublication(generator.getBrageRecord(), API_HOST);
+        var file = getFirstFile(publication);
+        var importUploadDetail = (ImportUploadDetails) file.getUploadDetails();
+        assertEquals(importUploadDetail.archive(), NPOLAR_SHORT_NAME);
+    }
+
+    private static File getFirstFile(Publication publication) {
+        return publication.getAssociatedArtifacts().stream()
+                   .filter(File.class::isInstance)
+                   .map(File.class::cast)
+                   .findFirst()
+                   .orElseThrow();
+    }
+
     private static Contributor randomContributorWithOrcId() {
         return new Contributor(new Identity(randomString(), randomString(), randomString()),
                                "ACTOR",
@@ -145,6 +176,8 @@ class BrageNvaMapperTest {
     }
 
     private ContentFile createRandomContentFileWithBundleType(BundleType bundleType) {
-        return new ContentFile(randomString(), bundleType, randomString(), UUID.randomUUID(), null, null);
+        return new ContentFile(randomString(), bundleType, randomString(), UUID.randomUUID(),
+                               new License(randomString(), new NvaLicense(randomUri())),
+                               null);
     }
 }
