ff934cb15ff240c80421a015ac98e94a315ff5d1
Kir Truhacev <kir.truhacev@sikt.no>
2024-09-17 15:10:27 +0200

    refactoring

--- publication-model/src/main/java/no/unit/nva/model/instancetypes/degree/UnconfirmedDocument.java
+++ publication-model/src/main/java/no/unit/nva/model/instancetypes/degree/UnconfirmedDocument.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public record UnconfirmedDocument(String text, Integer sequence) implements RelatedDocument{
+public record UnconfirmedDocument(String text, Integer sequence) implements RelatedDocument {
 
     public static UnconfirmedDocument fromValue(String value) {
         return new UnconfirmedDocument(value, null);
