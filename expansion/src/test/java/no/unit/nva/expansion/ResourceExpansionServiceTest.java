ad8445e19967ddaa2401d054fe665c520139fd67
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-10-09 15:25:37 +0200

    Remove stray imports

--- expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
+++ expansion/src/test/java/no/unit/nva/expansion/ResourceExpansionServiceTest.java
@@ -107,7 +107,6 @@ import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Named;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
--- tickets/src/test/java/no/unit/nva/publication/ticket/create/CreateTicketHandlerTest.java
+++ tickets/src/test/java/no/unit/nva/publication/ticket/create/CreateTicketHandlerTest.java
@@ -93,7 +93,6 @@ import nva.commons.logutils.LogUtils;
 import nva.commons.logutils.TestAppender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
