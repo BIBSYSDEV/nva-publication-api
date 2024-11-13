efbb80312f5697521ffc085223f1f8027019f869
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-11-13 15:19:37 +0100

    Rework test to look into arrays

--- expansion/src/test/java/no/unit/nva/expansion/JsonPropertyScraper.java
+++ expansion/src/test/java/no/unit/nva/expansion/JsonPropertyScraper.java
@@ -6,18 +6,27 @@ import java.util.Set;
 
 public class JsonPropertyScraper {
 
+    public static final Set<String> fields = new HashSet<>();
+
     private JsonPropertyScraper() {
         // NO-OP
     }
 
     public static Set<String> getAllProperties(JsonNode node) {
-        var fields = new HashSet<String>();
-        var iterator = node.fields();
-        while (iterator.hasNext()) {
-            var field = iterator.next();
-            fields.add(field.getKey());
-            if (field.getValue().isObject()) {
-                fields.addAll(getAllProperties(field.getValue()));
+        if (node.isObject()) {
+            var iterator = node.fields();
+            while (iterator.hasNext()) {
+                var field = iterator.next();
+                fields.add(field.getKey());
+                if (field.getValue().isObject() || field.getValue().isArray()) {
+                    fields.addAll(getAllProperties(field.getValue()));
+                }
+            }
+        } else if (node.isArray()) {
+            for (JsonNode element : node) {
+                if (element.isObject() || element.isArray()) {
+                    fields.addAll(getAllProperties(element));
+                }
             }
         }
         return fields;
