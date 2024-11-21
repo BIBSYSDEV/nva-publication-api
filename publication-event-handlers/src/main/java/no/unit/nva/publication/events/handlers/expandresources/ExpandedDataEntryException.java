37a3ea54ab87ef1d8bf35a47ebe183ba1d411c1f
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-11-21 11:08:18 +0100

    Np 48115 flaky test in fetch publication handler (#1943)
    * fix(publication-rest): Flaky test in FetchPublicationHandlerTest->shouldReturnPublicationWithHiddenFilesWhenUserIsCurator()
    
    * throwing custom exception in expandedDataEntriesHandler
    
    ---------
    
    Co-authored-by: Torbjørn Kvåle <torbjorn.kvale@sikt.no>

---
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/expandresources/ExpandedDataEntryException.java
@@ -0,0 +1,8 @@
+package no.unit.nva.publication.events.handlers.expandresources;
+
+public class ExpandedDataEntryException extends RuntimeException {
+
+    public ExpandedDataEntryException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
