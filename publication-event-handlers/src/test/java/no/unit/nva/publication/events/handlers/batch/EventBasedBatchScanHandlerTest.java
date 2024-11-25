dcfbe44cc1caf002aa7345270a1a4c1baedd939e
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2024-11-25 12:03:36 +0100

    removing curating institutions migration code (#1944)

--- publication-event-handlers/src/test/java/no/unit/nva/publication/events/handlers/batch/EventBasedBatchScanHandlerTest.java
+++ publication-event-handlers/src/test/java/no/unit/nva/publication/events/handlers/batch/EventBasedBatchScanHandlerTest.java
@@ -15,7 +15,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -52,7 +51,6 @@ import org.junit.jupiter.api.function.Executable;
 import org.mockito.ArgumentCaptor;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
-import software.amazon.awssdk.services.s3.S3Client;
 
 class EventBasedBatchScanHandlerTest extends ResourcesLocalTest {
 
@@ -69,8 +67,6 @@ class EventBasedBatchScanHandlerTest extends ResourcesLocalTest {
     private ResourceService resourceService;
     private TicketService ticketService;
     private AmazonDynamoDB dynamoDbClient;
-    private S3Client s3Client;
-    private Environment environment;
 
     @Override
     @BeforeEach
@@ -83,9 +79,7 @@ class EventBasedBatchScanHandlerTest extends ResourcesLocalTest {
         dynamoDbClient = super.client;
         this.resourceService = spy(getResourceServiceBuilder().build());
         this.ticketService = getTicketService();
-        this.s3Client = mock(S3Client.class);
-        this.environment = mock(Environment.class);
-        this.handler = new EventBasedBatchScanHandler(resourceService, eventBridgeClient, s3Client, environment);
+        this.handler = new EventBasedBatchScanHandler(resourceService, eventBridgeClient);
     }
 
     @Test
@@ -234,7 +228,7 @@ class EventBasedBatchScanHandlerTest extends ResourcesLocalTest {
         doThrow(new RuntimeException(expectedExceptionMessage)).when(spiedResourceService)
             .scanResources(anyInt(), any(), any());
 
-        handler = new EventBasedBatchScanHandler(spiedResourceService, eventBridgeClient, s3Client, environment);
+        handler = new EventBasedBatchScanHandler(spiedResourceService, eventBridgeClient);
         Executable action = () -> handler.handleRequest(createInitialScanRequest(ONE_ENTRY_PER_EVENT), output, context);
         assertThrows(RuntimeException.class, action);
         assertThat(logger.getMessages(), containsString(expectedExceptionMessage));
