5bc9ab575c0170a58378a804bf15244a4852be2f
Kir Truhacev <kir.truhacev@sikt.no>
2024-12-04 13:06:27 +0100

    updating to consume DataEntryUpdateEvent

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/log/PersistLogEntryEventHandler.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/log/PersistLogEntryEventHandler.java
@@ -1,15 +1,12 @@
 package no.unit.nva.publication.events.handlers.log;
 
 import static no.unit.nva.publication.events.handlers.PublicationEventsConfig.EVENTS_BUCKET;
-import static no.unit.nva.publication.events.handlers.fanout.DynamodbStreamRecordDaoMapper.toEntity;
 import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
-import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import java.util.Map;
 import java.util.Optional;
-import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.events.handlers.DestinationsEventBridgeEventHandler;
 import no.unit.nva.events.handlers.EventHandler;
+import no.unit.nva.events.models.AwsEventBridgeDetail;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
 import no.unit.nva.events.models.EventReference;
 import no.unit.nva.identifiers.SortableIdentifier;
@@ -21,13 +18,13 @@ import no.unit.nva.s3.S3Driver;
 import nva.commons.core.JacocoGenerated;
 import software.amazon.awssdk.services.s3.S3Client;
 
-public class PersistLogEntryEventHandler extends EventHandler<EventReference, Void> {
+public class PersistLogEntryEventHandler extends DestinationsEventBridgeEventHandler<EventReference, Void> {
 
     private final S3Client s3Client;
     private final ResourceService resourceService;
 
     @JacocoGenerated
-    protected PersistLogEntryEventHandler() {
+    public PersistLogEntryEventHandler() {
         this(S3Driver.defaultS3Client().build(), ResourceService.defaultService());
     }
 
@@ -38,13 +35,10 @@ public class PersistLogEntryEventHandler extends EventHandler<EventReference, Vo
     }
 
     @Override
-    protected Void processInput(EventReference eventReference, AwsEventBridgeEvent<EventReference> awsEventBridgeEvent,
-                                Context context) {
+    protected Void processInputPayload(EventReference eventReference,
+                                       AwsEventBridgeEvent<AwsEventBridgeDetail<EventReference>> awsEventBridgeEvent,
+                                       Context context) {
         return readNewImageResourceIdentifier(eventReference).map(this::handleNewImage).orElse(null);
-    }
-
-    private static DynamodbStreamRecord parseDynamoDbStreamRecord(String value) {
-        return attempt(() -> JsonUtils.dtoObjectMapper.readValue(value, DynamodbStreamRecord.class)).orElseThrow();
     }
 
     private Void handleNewImage(SortableIdentifier resourceIdentifier) {
@@ -56,26 +50,15 @@ public class PersistLogEntryEventHandler extends EventHandler<EventReference, Vo
         return null;
     }
 
-    private Optional<SortableIdentifier> readNewImageResourceIdentifier(EventReference input) {
-        return attempt(() -> fetchDynamoDbStreamRecord(input)).map(
-                PersistLogEntryEventHandler::parseDynamoDbStreamRecord)
-                   .map(this::convertToDataEntryUpdateEvent)
+    private Optional<SortableIdentifier> readNewImageResourceIdentifier(EventReference eventReference) {
+        return attempt(() -> fetchDynamoDbStreamRecord(eventReference))
+                   .map(DataEntryUpdateEvent::fromJson)
                    .map(DataEntryUpdateEvent::getNewData)
                    .map(Entity::getIdentifier)
                    .toOptional();
     }
 
-    private String fetchDynamoDbStreamRecord(EventReference input) {
-        return new S3Driver(s3Client, EVENTS_BUCKET).readEvent(input.getUri());
-    }
-
-    private Entity getEntity(Map<String, AttributeValue> image) {
-        return attempt(() -> toEntity(image)).orElse(failure -> Optional.<Entity>empty()).orElse(null);
-    }
-
-    private DataEntryUpdateEvent convertToDataEntryUpdateEvent(DynamodbStreamRecord dynamoDbRecord) {
-        return new DataEntryUpdateEvent(dynamoDbRecord.getEventName(),
-                                        getEntity(dynamoDbRecord.getDynamodb().getOldImage()),
-                                        getEntity(dynamoDbRecord.getDynamodb().getNewImage()));
+    private String fetchDynamoDbStreamRecord(EventReference eventReference) {
+        return new S3Driver(s3Client, EVENTS_BUCKET).readEvent(eventReference.getUri());
     }
 }
