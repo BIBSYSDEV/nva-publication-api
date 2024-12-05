92d84acbc578574aad22cac65516940cbb052bca
Kir Truhacev <kir.truhacev@sikt.no>
2024-12-05 14:40:48 +0100

    code review

--- publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogResponse.java
+++ publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogResponse.java
@@ -3,6 +3,7 @@ package no.unit.nva.publication.log.rest;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.Collection;
 import java.util.List;
 import no.unit.nva.publication.model.business.logentry.LogEntry;
 
@@ -12,7 +13,7 @@ public record PublicationLogResponse(List<LogEntryDto> logEntries) {
 
     public static final String TYPE = "PublicationLog";
 
-    public static PublicationLogResponse fromLogEntries(List<LogEntry> logEntries) {
+    public static PublicationLogResponse fromLogEntries(Collection<LogEntry> logEntries) {
         var logEntriesDto = logEntries.stream().map(LogEntryDto::fromLogEntry).toList();
         return new PublicationLogResponse(logEntriesDto);
     }
