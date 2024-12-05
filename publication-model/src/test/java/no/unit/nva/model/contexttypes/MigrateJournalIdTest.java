c69d719ea13e5f4c9ce4dd86dcf239628c70aa60
monaullah <monaullah98@gmail.com>
2024-12-05 09:13:34 +0100

    shouldLogUriIfOldIdHasUnexpectedPath

--- publication-model/src/test/java/no/unit/nva/model/contexttypes/MigrateJournalIdTest.java
+++ publication-model/src/test/java/no/unit/nva/model/contexttypes/MigrateJournalIdTest.java
@@ -2,9 +2,11 @@ package no.unit.nva.model.contexttypes;
 
 import static no.unit.nva.utils.MigrateSerialPublicationsUtil.constructExampleIdWithPath;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import nva.commons.core.paths.UriWrapper;
+import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.Test;
 
 @Deprecated
@@ -18,5 +20,15 @@ public class MigrateJournalIdTest {
                                 .getUri();
         var journal = new Journal(oldId);
         assertThat(journal.getId(), is(equalTo(expectedNewId)));
+    }
+
+    @Test
+    void shouldLogUriIfOldIdHasUnexpectedPath() {
+        var oldId = constructExampleIdWithPath("unexpected-path");
+        var appender = LogUtils.getTestingAppender(Journal.class);
+
+        new Journal(oldId);
+
+        assertThat(appender.getMessages(), containsString(oldId.toString()));
     }
 }
--- publication-model/src/test/java/no/unit/nva/model/contexttypes/MigrateSeriesIdTest.java
+++ publication-model/src/test/java/no/unit/nva/model/contexttypes/MigrateSeriesIdTest.java
@@ -2,9 +2,11 @@ package no.unit.nva.model.contexttypes;
 
 import static no.unit.nva.utils.MigrateSerialPublicationsUtil.constructExampleIdWithPath;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import nva.commons.core.paths.UriWrapper;
+import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.Test;
 
 @Deprecated
@@ -18,5 +20,15 @@ public class MigrateSeriesIdTest {
                                 .getUri();
         var series = new Series(oldId);
         assertThat(series.getId(), is(equalTo(expectedNewId)));
+    }
+
+    @Test
+    void shouldLogUriIfOldIdHasUnexpectedPath() {
+        var oldId = constructExampleIdWithPath("unexpected-path");
+        var appender = LogUtils.getTestingAppender(Series.class);
+
+        new Series(oldId);
+
+        assertThat(appender.getMessages(), containsString(oldId.toString()));
     }
 }
