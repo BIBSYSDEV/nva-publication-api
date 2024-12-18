232dc77fc0f6e56ca59d0bed8895ac0e8117cb7f
Kir Truhacev <kir.truhacev@sikt.no>
2024-12-18 08:44:41 +0100

    moving multipart-upload handlers to publication-api

---
+++ publication-rest/src/main/java/no/unit/nva/publication/uploadfile/restmodel/CompleteUploadPart.java
@@ -0,0 +1,22 @@
+package no.unit.nva.publication.uploadfile.restmodel;
+
+import static java.util.Objects.requireNonNull;
+import com.amazonaws.services.s3.model.PartETag;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record CompleteUploadPart(@JsonProperty("PartNumber") Integer partNumber, @JsonProperty("ETag") String etag) {
+
+    public static PartETag toPartETag(CompleteUploadPart completeUploadPart) {
+        return new PartETag(completeUploadPart.partNumber(), completeUploadPart.etag());
+    }
+
+    public boolean hasValue() {
+        try {
+            requireNonNull(etag);
+            requireNonNull(partNumber);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ publication-rest/src/main/java/no/unit/nva/publication/uploadfile/restmodel/ListPartsResponseBody.java
@@ -0,0 +1,7 @@
+package no.unit.nva.publication.uploadfile.restmodel;
+
+import java.util.List;
+
+public record ListPartsResponseBody(List<ListPartsElement> listParts) {
+
+}
