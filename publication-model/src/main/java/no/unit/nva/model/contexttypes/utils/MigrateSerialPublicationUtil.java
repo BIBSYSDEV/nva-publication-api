4ca77488b89d893f225f08b80a4829b42d73dfd8
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-12-19 08:41:41 +0100

    Np 48011 Migrate journal and series ids - allow old paths journal and series for both types (#1975)
    * shouldUpdateOldJournalIdWithNewPathSerialPublicationWhenOldPathIsSeries
    
    * shouldUpdateOldSeriesIdWithNewPathSerialPublicationWhenOldPathIsJournal
    
    * Update migration code

--- publication-model/src/main/java/no/unit/nva/model/contexttypes/utils/MigrateSerialPublicationUtil.java
+++ publication-model/src/main/java/no/unit/nva/model/contexttypes/utils/MigrateSerialPublicationUtil.java
@@ -12,25 +12,32 @@ public class MigrateSerialPublicationUtil {
 
     public static final Logger logger = LoggerFactory.getLogger(MigrateSerialPublicationUtil.class.getName());
     private static final String NEW_PATH_SERIAL_PUBLICATION = "serial-publication";
+    private static final String OLD_PATH_JOURNAL = "journal";
+    private static final String OLD_PATH_SERIES = "series";
     private static final int INDEX_FROM_END_CHANNEL_TYPE_PATH_ELEMENT = 2;
 
     @Deprecated
-    public static URI migratePath(URI id, String pathToMigrate) {
+    public static URI migratePath(URI id) {
         var oldPath = attempt(() -> UriWrapper.fromUri(id)
                                         .getPath()
                                         .getPathElementByIndexFromEnd(INDEX_FROM_END_CHANNEL_TYPE_PATH_ELEMENT))
                           .toOptional();
-        return oldPath.filter(path -> path.equals(pathToMigrate))
+        return oldPath.filter(MigrateSerialPublicationUtil::isDeprecated)
                    .map(uri -> replaceOldPath(id))
-                   .orElseGet(() -> isAlreadyMigrated(oldPath) ? id : logUnexpectedUri(id, pathToMigrate));
+                   .orElseGet(() -> isAlreadyMigrated(oldPath) ? id : logUnexpectedUri(id));
+    }
+
+    private static boolean isDeprecated(String path) {
+        return OLD_PATH_JOURNAL.equals(path) || OLD_PATH_SERIES.equals(path);
     }
 
     private static boolean isAlreadyMigrated(Optional<String> oldPath) {
         return oldPath.isPresent() && NEW_PATH_SERIAL_PUBLICATION.equals(oldPath.get());
     }
 
-    private static URI logUnexpectedUri(URI id, String oldPath) {
-        logger.info(String.format("Unexpected URI. Expected id with path: %s, but found id: %s", oldPath, id));
+    private static URI logUnexpectedUri(URI id) {
+        logger.info(String.format("Unexpected URI. Expected id with path: %s or %s, but found id: %s",
+                                  OLD_PATH_JOURNAL, OLD_PATH_SERIES, id));
         return id;
     }
 
