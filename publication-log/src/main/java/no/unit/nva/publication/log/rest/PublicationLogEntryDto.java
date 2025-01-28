1132fd0d7fc2e710f24e1b26821356bc3ce456ee
Kir Truhacev <kir.truhacev@sikt.no>
2025-01-28 15:22:51 +0100

    adding resource and file identifier to log entries

--- publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogEntryDto.java
+++ publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogEntryDto.java
@@ -3,17 +3,20 @@ package no.unit.nva.publication.log.rest;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.time.Instant;
+import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.publication.model.business.logentry.LogTopic;
 import no.unit.nva.publication.model.business.logentry.LogUser;
 import no.unit.nva.publication.model.business.logentry.PublicationLogEntry;
 
 @JsonTypeName(PublicationLogEntryDto.TYPE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public record PublicationLogEntryDto(LogTopic topic, Instant timestamp, LogUser performedBy) implements LogEntryDto {
+public record PublicationLogEntryDto(LogTopic topic, Instant timestamp, LogUser performedBy,
+                                     SortableIdentifier publicationIdentifier) implements LogEntryDto {
 
     public static final String TYPE = "PublicationLogEntry";
 
     public static PublicationLogEntryDto fromLogEntry(PublicationLogEntry publicationLogEntry) {
-        return new PublicationLogEntryDto(publicationLogEntry.topic(), publicationLogEntry.timestamp(), publicationLogEntry.performedBy());
+        return new PublicationLogEntryDto(publicationLogEntry.topic(), publicationLogEntry.timestamp(),
+                                          publicationLogEntry.performedBy(), publicationLogEntry.resourceIdentifier());
     }
 }
