16b41f0c7731dc6e1f3b92225e7e1e678b82ceec
Kir Truhacev <kir.truhacev@sikt.no>
2025-02-13 12:21:43 +0100

    adding import details to dto

--- publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogEntryDto.java
+++ publication-log/src/main/java/no/unit/nva/publication/log/rest/PublicationLogEntryDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.time.Instant;
 import no.unit.nva.identifiers.SortableIdentifier;
+import no.unit.nva.model.ImportSource;
 import no.unit.nva.publication.model.business.logentry.LogTopic;
 import no.unit.nva.publication.model.business.logentry.LogUser;
 import no.unit.nva.publication.model.business.logentry.PublicationLogEntry;
@@ -11,12 +12,14 @@ import no.unit.nva.publication.model.business.logentry.PublicationLogEntry;
 @JsonTypeName(PublicationLogEntryDto.TYPE)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 public record PublicationLogEntryDto(LogTopic topic, Instant timestamp, LogUser performedBy,
-                                     SortableIdentifier publicationIdentifier) implements LogEntryDto {
+                                     SortableIdentifier publicationIdentifier, ImportSource importSource)
+    implements LogEntryDto {
 
     public static final String TYPE = "PublicationLogEntry";
 
     public static PublicationLogEntryDto fromLogEntry(PublicationLogEntry publicationLogEntry) {
         return new PublicationLogEntryDto(publicationLogEntry.topic(), publicationLogEntry.timestamp(),
-                                          publicationLogEntry.performedBy(), publicationLogEntry.resourceIdentifier());
+                                          publicationLogEntry.performedBy(), publicationLogEntry.resourceIdentifier(),
+                                          publicationLogEntry.importSource());
     }
 }
