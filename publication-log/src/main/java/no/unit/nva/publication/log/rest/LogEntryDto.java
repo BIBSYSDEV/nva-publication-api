a59be3612ea869cac1a8cf626f5051ec4b770257
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2025-02-15 10:06:02 +0100

    fixing json annotation on ticket log entry dto (#2055)

--- publication-log/src/main/java/no/unit/nva/publication/log/rest/LogEntryDto.java
+++ publication-log/src/main/java/no/unit/nva/publication/log/rest/LogEntryDto.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import java.time.Instant;
+import no.unit.nva.publication.model.business.logentry.FileLogEntry;
 import no.unit.nva.publication.model.business.logentry.LogTopic;
 import no.unit.nva.publication.model.business.logentry.LogUser;
+import no.unit.nva.publication.model.business.logentry.PublicationLogEntry;
+import no.unit.nva.publication.model.business.logentry.TicketLogEntry;
 
 @JsonTypeInfo(use = Id.NAME, property = "type")
-@JsonSubTypes({@JsonSubTypes.Type(name = FileLogEntryDto.TYPE, value = FileLogEntryDto.class),
-    @JsonSubTypes.Type(name = PublicationLogEntryDto.TYPE, value = PublicationLogEntryDto.class),
+@JsonSubTypes({@JsonSubTypes.Type(name = FileLogEntry.TYPE, value = FileLogEntryDto.class),
+    @JsonSubTypes.Type(name = PublicationLogEntry.TYPE, value = PublicationLogEntryDto.class),
+    @JsonSubTypes.Type(name = TicketLogEntry.TYPE, value = TicketLogEntryDto.class)
     })
 public interface LogEntryDto {
 
