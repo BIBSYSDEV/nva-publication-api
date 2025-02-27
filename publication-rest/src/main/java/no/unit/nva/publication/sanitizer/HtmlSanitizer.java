09bd13018c5a928e727b227b28dfc15de62f5617
Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>
2025-02-27 08:55:21 +0100

    Move constructor below static field to please codacy

--- publication-rest/src/main/java/no/unit/nva/publication/sanitizer/HtmlSanitizer.java
+++ publication-rest/src/main/java/no/unit/nva/publication/sanitizer/HtmlSanitizer.java
@@ -5,14 +5,14 @@ import org.owasp.html.PolicyFactory;
 
 public final class HtmlSanitizer {
 
-    private HtmlSanitizer() {}
-
     private static final PolicyFactory POLICY_FACTORY = new HtmlPolicyBuilder()
             .allowElements("a", "i", "p", "br", "b", "strong", "em")
             .allowAttributes("href", "target").onElements("a")
             .allowUrlProtocols("https", "http")
             .requireRelNofollowOnLinks() // Adds rel=nofollow to all links
             .toFactory();
+
+    private HtmlSanitizer() {}
 
     public static String sanitize(String input) {
         return POLICY_FACTORY.sanitize(input);
