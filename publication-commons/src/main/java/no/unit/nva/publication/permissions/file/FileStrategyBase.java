8a3d346a4ad434afb81c6e2c8b6afa72adeb3389
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-02-28 14:52:41 +0100

    feedback

--- publication-commons/src/main/java/no/unit/nva/publication/permissions/file/FileStrategyBase.java
+++ publication-commons/src/main/java/no/unit/nva/publication/permissions/file/FileStrategyBase.java
@@ -53,7 +53,7 @@ public class FileStrategyBase {
         return file.getOwnerAffiliation().equals(userTopLevelOrg);
     }
 
-    private boolean hasTopLevelRelationForResourceContributors() {
+    private boolean userBelongsToCuratingInstitution() {
         var userTopLevelOrg = userInstance.getTopLevelOrgCristinId();
 
         logger.info("checking if resource top level affiliation {} for user {} is equal to {}.",
@@ -65,16 +65,16 @@ public class FileStrategyBase {
         return resource.getCuratingInstitutions().stream().anyMatch(org -> org.id().equals(userTopLevelOrg));
     }
 
-    private boolean hasCuratorRelation() {
-        return hasTopLevelRelationForResourceContributors() || hasTopLevelRelationForResourceOwner();
+    private boolean userRelatesToPublicationThroughPublicationOwnerOrCuratingInstitution() {
+        return userIsFromSameInstitutionAsPublicationOwner() || userBelongsToCuratingInstitution();
     }
 
-    private boolean hasTopLevelRelationForResourceOwner() {
+    private boolean userIsFromSameInstitutionAsPublicationOwner() {
         return resource.getResourceOwner().getOwnerAffiliation().equals(userInstance.getTopLevelOrgCristinId());
     }
 
     protected boolean currentUserIsFileCurator() {
-        return hasAccessRight(AccessRight.MANAGE_RESOURCE_FILES) && hasCuratorRelation();
+        return hasAccessRight(AccessRight.MANAGE_RESOURCE_FILES) && userRelatesToPublicationThroughPublicationOwnerOrCuratingInstitution();
     }
 
     protected boolean currentUserIsContributor() {
@@ -126,7 +126,7 @@ public class FileStrategyBase {
     }
 
     protected boolean currentUserIsDegreeEmbargoFileCuratorForGivenFile() {
-        return hasAccessRight(MANAGE_DEGREE_EMBARGO) && hasCuratorRelation();
+        return hasAccessRight(MANAGE_DEGREE_EMBARGO) && userRelatesToPublicationThroughPublicationOwnerOrCuratingInstitution();
     }
 
     protected boolean currentUserIsDegreeFileCuratorForGivenFile() {
