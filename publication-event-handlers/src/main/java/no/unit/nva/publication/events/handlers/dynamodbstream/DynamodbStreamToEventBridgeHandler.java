1e8352e69bc778c0f1ff1f878f06744439b54d4a
Kir Truhacev <kir.truhacev@sikt.no>
2025-03-24 10:41:38 +0100

    code review

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandler.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/handlers/dynamodbstream/DynamodbStreamToEventBridgeHandler.java
@@ -47,13 +47,13 @@ import software.amazon.awssdk.services.s3.S3Client;
  */
 public class DynamodbStreamToEventBridgeHandler implements RequestHandler<DynamodbEvent, EventReference> {
 
-    public static final String PROCEEDING_EVENT_MESSAGE = "Proceeding event for identifier: {}";
-    public static final String DETAIL_TYPE_NOT_IMPORTANT = "See event topic";
-    public static final String EMITTED_EVENT_MESSAGE = "Emitted Event:{}";
-    public static final String SENT_TO_RECOVERY_QUEUE_MESSAGE = "DateEntry has been sent to recovery queue: {}";
-    public static final EventReference DO_NOT_EMIT_EVENT = null;
-    public static final String DYNAMO_DB_STREAM_SOURCE = "DynamoDbStream";
-    public static final String RECOVERY_QUEUE = new Environment().readEnv("RECOVERY_QUEUE");
+    private static final String PROCESSING_EVENT_MESSAGE = "Processing event for identifier: {}";
+    private static final String DETAIL_TYPE_NOT_IMPORTANT = "See event topic";
+    private static final String EMITTED_EVENT_MESSAGE = "Emitted Event:{}";
+    private static final String SENT_TO_RECOVERY_QUEUE_MESSAGE = "DateEntry has been sent to recovery queue: {}";
+    private static final EventReference DO_NOT_EMIT_EVENT = null;
+    private static final String DYNAMO_DB_STREAM_SOURCE = "DynamoDbStream";
+    private static final String RECOVERY_QUEUE = new Environment().readEnv("RECOVERY_QUEUE");
     private static final Logger logger = LoggerFactory.getLogger(DynamodbStreamToEventBridgeHandler.class);
     private final S3Driver s3Driver;
     private final QueueClient sqsClient;
@@ -101,7 +101,7 @@ public class DynamodbStreamToEventBridgeHandler implements RequestHandler<Dynamo
     }
 
     private EventReference sendEvent(DataEntryUpdateEvent blob, Context context) {
-        logger.info(PROCEEDING_EVENT_MESSAGE, getIdentifier(blob));
+        logger.info(PROCESSING_EVENT_MESSAGE, getIdentifier(blob));
         return attempt(() -> saveBlobToS3(blob)).map(blobUri -> new EventReference(blob.getTopic(), blobUri))
                    .map(eventReference -> sendEvent(eventReference, context))
                    .orElse(failure -> processRecoveryMessage(failure, blob));
