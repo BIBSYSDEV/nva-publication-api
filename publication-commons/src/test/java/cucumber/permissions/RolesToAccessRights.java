c85d31bfaf7bf8cab05ad9a2d477e5436c51879b
Joachim Jørgensen <joachim.jorgensen@Joachims-MacBook-Pro.local>
2025-06-03 13:25:30 +0200

    Undo removal of role to access rights mappings still required by file permission test

--- publication-commons/src/test/java/cucumber/permissions/RolesToAccessRights.java
+++ publication-commons/src/test/java/cucumber/permissions/RolesToAccessRights.java
@@ -4,7 +4,13 @@ import static cucumber.permissions.PermissionsRole.CREATOR;
 import static cucumber.permissions.PermissionsRole.DOI_CURATOR;
 import static cucumber.permissions.PermissionsRole.EDITOR;
 import static cucumber.permissions.PermissionsRole.EMBARGO_THESIS_CURATOR;
+import static cucumber.permissions.PermissionsRole.FILE_CURATOR_BY_CONTRIBUTOR_FOR_OTHERS;
+import static cucumber.permissions.PermissionsRole.FILE_CURATOR_BY_PUBLICATION_OWNER;
+import static cucumber.permissions.PermissionsRole.FILE_CURATOR_DEGREE;
+import static cucumber.permissions.PermissionsRole.FILE_CURATOR_DEGREE_EMBARGO;
+import static cucumber.permissions.PermissionsRole.FILE_CURATOR_FOR_GIVEN_FILE;
 import static cucumber.permissions.PermissionsRole.INTERNAL_IMPORTER;
+import static cucumber.permissions.PermissionsRole.NON_DEGREE_CURATOR_TYPE;
 import static cucumber.permissions.PermissionsRole.NVI_CURATOR;
 import static cucumber.permissions.PermissionsRole.PUBLISHING_CURATOR;
 import static cucumber.permissions.PermissionsRole.SUPPORT_CURATOR;
@@ -14,6 +20,7 @@ import static nva.commons.apigateway.AccessRight.MANAGE_DEGREE;
 import static nva.commons.apigateway.AccessRight.MANAGE_DEGREE_EMBARGO;
 import static nva.commons.apigateway.AccessRight.MANAGE_DOI;
 import static nva.commons.apigateway.AccessRight.MANAGE_IMPORT;
+import static nva.commons.apigateway.AccessRight.MANAGE_NVI;
 import static nva.commons.apigateway.AccessRight.MANAGE_NVI_CANDIDATES;
 import static nva.commons.apigateway.AccessRight.MANAGE_OWN_AFFILIATION;
 import static nva.commons.apigateway.AccessRight.MANAGE_OWN_RESOURCES;
@@ -28,25 +35,39 @@ import nva.commons.apigateway.AccessRight;
 
 public final class RolesToAccessRights {
 
-    public static final Map<PermissionsRole, Set<AccessRight>> roleToAccessRightsMap = Map.of(
-        PUBLISHING_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                                   MANAGE_PUBLISHING_REQUESTS,
-                                   MANAGE_RESOURCE_FILES),
-        NVI_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                            MANAGE_NVI_CANDIDATES),
-        DOI_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                            MANAGE_DOI),
-        SUPPORT_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                                SUPPORT),
-        CREATOR, Set.of(MANAGE_OWN_RESOURCES),
-        INTERNAL_IMPORTER, Set.of(MANAGE_IMPORT),
-        THESIS_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                               MANAGE_DEGREE),
-        EMBARGO_THESIS_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
-                                       MANAGE_DEGREE_EMBARGO),
-        EDITOR, Set.of(MANAGE_OWN_AFFILIATION,
-                       MANAGE_RESOURCES_ALL,
-                       MANAGE_CHANNEL_CLAIMS));
+    public static final Map<PermissionsRole, Set<AccessRight>> roleToAccessRightsMap = Map.ofEntries(
+        Map.entry(PUBLISHING_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                             MANAGE_PUBLISHING_REQUESTS,
+                                             MANAGE_RESOURCE_FILES)),
+        Map.entry(NVI_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                      MANAGE_NVI_CANDIDATES)),
+        Map.entry(DOI_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                      MANAGE_DOI)),
+        Map.entry(SUPPORT_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                          SUPPORT)),
+        Map.entry(CREATOR, Set.of(MANAGE_OWN_RESOURCES)),
+        Map.entry(INTERNAL_IMPORTER, Set.of(MANAGE_IMPORT)),
+        Map.entry(THESIS_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                         MANAGE_DEGREE)),
+        Map.entry(EMBARGO_THESIS_CURATOR, Set.of(MANAGE_RESOURCES_STANDARD,
+                                                 MANAGE_DEGREE_EMBARGO)),
+        Map.entry(EDITOR, Set.of(MANAGE_OWN_AFFILIATION,
+                                 MANAGE_RESOURCES_ALL,
+                                 MANAGE_CHANNEL_CLAIMS)),
 
-    private RolesToAccessRights() {}
+        // Old entries to be removed
+        Map.entry(FILE_CURATOR_BY_CONTRIBUTOR_FOR_OTHERS, Set.of(MANAGE_RESOURCES_STANDARD, MANAGE_RESOURCE_FILES)),
+        Map.entry(FILE_CURATOR_BY_PUBLICATION_OWNER, Set.of(MANAGE_RESOURCES_STANDARD, MANAGE_RESOURCE_FILES)),
+        Map.entry(FILE_CURATOR_DEGREE_EMBARGO, Set.of(MANAGE_DEGREE, MANAGE_DEGREE_EMBARGO, MANAGE_RESOURCES_STANDARD)),
+        Map.entry(FILE_CURATOR_DEGREE, Set.of(MANAGE_DEGREE, MANAGE_RESOURCES_STANDARD)),
+        Map.entry(FILE_CURATOR_FOR_GIVEN_FILE, Set.of(MANAGE_RESOURCES_STANDARD, MANAGE_RESOURCE_FILES)),
+        Map.entry(NON_DEGREE_CURATOR_TYPE, Set.of(MANAGE_RESOURCES_STANDARD,
+                                                  MANAGE_DOI, SUPPORT,
+                                                  MANAGE_NVI,
+                                                  MANAGE_PUBLISHING_REQUESTS,
+                                                  MANAGE_RESOURCE_FILES))
+    );
+
+    private RolesToAccessRights() {
+    }
 }
