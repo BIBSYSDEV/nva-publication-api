07193a40c910024352634090648e8b6a66595e85
brinxmat <789709+brinxmat@users.noreply.github.com>
2025-09-22 16:19:23 +0200

    Make funding source labels optional

--- expansion/src/main/java/no/unit/nva/expansion/utils/FramedJsonGenerator.java
+++ expansion/src/main/java/no/unit/nva/expansion/utils/FramedJsonGenerator.java
@@ -129,11 +129,11 @@ public class FramedJsonGenerator {
                 [] project:funding ?funding .
                 ?funding a ?rawType ;
                     project:source ?source ;
-                    project:identifier ?identifier ;
-                    project:label ?label .
-                 OPTIONAL {   ?source a nva:FundingSource . }
-                 OPTIONAL {   ?source nva:identifier ?sourceIdentifier . }
-                 OPTIONAL {   ?source nva:label ?sourceLabel . }
+                    project:identifier ?identifier .
+                 OPTIONAL { ?funding project:label ?label . }
+                 OPTIONAL { ?source a nva:FundingSource . }
+                 OPTIONAL { ?source nva:identifier ?sourceIdentifier . }
+                 OPTIONAL { ?source nva:label ?sourceLabel . }
               } UNION {
                 [] nva:funding ?funding .
                 ?funding a ?rawType ;
