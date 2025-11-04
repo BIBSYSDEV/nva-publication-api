a313d9980403d83c38d8bac96e8893ed221452b8
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2025-11-04 13:36:36 +0100

    fix: Remove more logging

--- expansion/src/main/java/no/unit/nva/expansion/ResourceExpansionServiceImpl.java
+++ expansion/src/main/java/no/unit/nva/expansion/ResourceExpansionServiceImpl.java
@@ -133,9 +133,7 @@ public class ResourceExpansionServiceImpl implements ResourceExpansionService {
 
     private Optional<ExpandedDataEntry> expandResource(Resource resource, boolean useUriContext)
         throws JsonProcessingException {
-        logger.info("Expanding Resource: {} - {}",
-                    resource.getIdentifier(),
-                    attempt(() -> JsonUtils.dtoObjectMapper.writeValueAsString(resource)).orElseThrow());
+        logger.info("Expanding Resource: {}", resource.getIdentifier());
         var expandedResource = ExpandedResource.fromPublication(uriRetriever,
                                                                 resourceService,
                                                                 queueClient,
