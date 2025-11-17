bc998cec18afd717675950aa9585b18a323adbce
Kir Truhacev <kir.truhacev@sikt.no>
2025-11-17 13:03:30 +0100

    removing unused interface

--- publication-commons/src/main/java/no/unit/nva/publication/model/storage/DatabaseEntryWithIdentifier.java
+++
@@ -1,8 +0,0 @@
-package no.unit.nva.publication.model.storage;
-
-import no.unit.nva.identifiers.SortableIdentifier;
-
-public interface DatabaseEntryWithIdentifier {
-
-    SortableIdentifier getIdentifier();
-}
