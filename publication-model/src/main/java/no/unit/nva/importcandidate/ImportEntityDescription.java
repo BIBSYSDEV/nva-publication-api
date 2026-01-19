3201ea6afab552ee719094dfd2987b339562f65d
Kir Truhacev <kir.truhacev@sikt.no>
2026-01-19 13:52:57 +0100

    applying contributor sequence rules to scopus contributors

--- publication-model/src/main/java/no/unit/nva/importcandidate/ImportEntityDescription.java
+++ publication-model/src/main/java/no/unit/nva/importcandidate/ImportEntityDescription.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import java.net.URI;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
 import no.unit.nva.model.PublicationDate;
 import no.unit.nva.model.Reference;
 
@@ -19,8 +22,32 @@ public record ImportEntityDescription(String mainTitle, URI language, Publicatio
                                       String description, Reference reference) {
 
     public ImportEntityDescription {
-        contributors = nonNull(contributors) ? List.copyOf(contributors) : Collections.emptyList();
+        contributors =
+            nonNull(contributors) ? List.copyOf(updateSequenceNumber(contributors)) : Collections.emptyList();
         tags = nonNull(tags) ? List.copyOf(tags) : Collections.emptyList();
-        alternativeAbstracts = nonNull(alternativeAbstracts) ? Map.copyOf(alternativeAbstracts) : Collections.emptyMap();
+        alternativeAbstracts =
+            nonNull(alternativeAbstracts) ? Map.copyOf(alternativeAbstracts) : Collections.emptyMap();
+    }
+
+    private static ImportContributor updateContributorWithSequence(ImportContributor contributor, int sequenceCounter) {
+        return new ImportContributor(contributor.identity(), contributor.affiliations(), contributor.role(),
+                                     sequenceCounter, contributor.correspondingAuthor());
+    }
+
+    private List<ImportContributor> updateSequenceNumber(Collection<ImportContributor> contributors) {
+        var contributorList = contributors.stream()
+                                  .filter(Objects::nonNull)
+                                  .sorted(Comparator.comparing(ImportContributor::sequence,
+                                                               Comparator.nullsLast(Comparator.naturalOrder())))
+                                  .toList();
+
+        return updatedContributorSequence(contributorList);
+    }
+
+    private List<ImportContributor> updatedContributorSequence(List<ImportContributor> contributorList) {
+        return IntStream.range(0, contributorList.size())
+                   .mapToObj(sequenceCounter -> updateContributorWithSequence(contributorList.get(sequenceCounter),
+                                                                              sequenceCounter + 1))
+                   .toList();
     }
 }
