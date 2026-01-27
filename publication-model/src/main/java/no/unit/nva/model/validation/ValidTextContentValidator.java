1b3fcbc5d33cbcf2d7c987f2603578e0eb396d34
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-01-27 11:09:09 +0100

    style(validation): remove unnecessary parentheses #NP-50552

--- publication-model/src/main/java/no/unit/nva/model/validation/ValidTextContentValidator.java
+++ publication-model/src/main/java/no/unit/nva/model/validation/ValidTextContentValidator.java
@@ -23,8 +23,8 @@ public class ValidTextContentValidator implements ConstraintValidator<ValidTextC
         return codePoint == 0x9
                || codePoint == 0xA
                || codePoint == 0xD
-               || (codePoint >= 0x20 && codePoint <= 0xD7FF)
-               || (codePoint >= 0xE000 && codePoint <= 0xFFFD)
-               || (codePoint >= 0x10000 && codePoint <= 0x10FFFF);
+               || codePoint >= 0x20 && codePoint <= 0xD7FF
+               || codePoint >= 0xE000 && codePoint <= 0xFFFD
+               || codePoint >= 0x10000 && codePoint <= 0x10FFFF;
     }
 }
