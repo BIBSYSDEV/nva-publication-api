ebdc325cae3b9e868bd3c699a4b1c9fbe6a39374
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-02-11 09:10:46 +0100

    UriWrapper.fromHost

--- publication-rest/src/main/java/no/unit/nva/publication/create/pia/PiaClient.java
+++ publication-rest/src/main/java/no/unit/nva/publication/create/pia/PiaClient.java
@@ -24,7 +24,6 @@ public class PiaClient {
     private static final String BASIC_AUTHORIZATION = "Basic %s";
 
     private static final String AUTHORIZATION = "Authorization";
-    private static final String HTTPS_SCHEME = "https://";
     private static final String SENTRALIMPORT = "sentralimport";
     private static final String AUTHORS = "authors";
     private final URI piaUri;
@@ -32,7 +31,7 @@ public class PiaClient {
     private final String authorization;
 
     public PiaClient(PiaClientConfig config) {
-        this.piaUri = UriWrapper.fromUri(HTTPS_SCHEME + config.piaHost())
+        this.piaUri = UriWrapper.fromHost(config.piaHost())
                           .addChild(SENTRALIMPORT)
                           .addChild(AUTHORS)
                           .getUri();
