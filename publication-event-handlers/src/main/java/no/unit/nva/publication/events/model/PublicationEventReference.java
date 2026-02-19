e8a421e825be6ef44f632532b8195b22f3b2d853
Kir Truhacev <kir.truhacev@sikt.no>
2026-02-19 13:55:28 +0100

    adding equals and hashcode to PublicationEventReference

--- publication-event-handlers/src/main/java/no/unit/nva/publication/events/model/PublicationEventReference.java
+++ publication-event-handlers/src/main/java/no/unit/nva/publication/events/model/PublicationEventReference.java
@@ -3,9 +3,11 @@ package no.unit.nva.publication.events.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.net.URI;
+import java.util.Objects;
 import no.unit.nva.events.models.EventReference;
 import no.unit.nva.identifiers.SortableIdentifier;
 import no.unit.nva.model.PublicationStatus;
+import nva.commons.core.JacocoGenerated;
 
 public class PublicationEventReference extends EventReference {
 
@@ -14,8 +16,7 @@ public class PublicationEventReference extends EventReference {
     private final PublicationStatus status;
 
     @JsonCreator
-    public PublicationEventReference(@JsonProperty("topic") String topic,
-                                     @JsonProperty("uri") URI uri,
+    public PublicationEventReference(@JsonProperty("topic") String topic, @JsonProperty("uri") URI uri,
                                      @JsonProperty("identifier") SortableIdentifier identifier,
                                      @JsonProperty("type") String type,
                                      @JsonProperty("status") PublicationStatus status) {
@@ -23,6 +24,26 @@ public class PublicationEventReference extends EventReference {
         this.identifier = identifier;
         this.type = type;
         this.status = status;
+    }
+
+    @JacocoGenerated
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PublicationEventReference that)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        return Objects.equals(getIdentifier(), that.getIdentifier())
+               && Objects.equals(getType(), that.getType())
+               && getStatus() == that.getStatus();
+    }
+
+    @JacocoGenerated
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getIdentifier(), getType(), getStatus());
     }
 
     public SortableIdentifier getIdentifier() {
