f8db1170fdbb49dcb07a4482821d22e8b770068a
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2026-04-22 11:22:14 +0200

    feedback

--- publication-model/src/main/java/no/unit/nva/model/instancetypes/researchdata/SoftwareSourceCode.java
+++ publication-model/src/main/java/no/unit/nva/model/instancetypes/researchdata/SoftwareSourceCode.java
@@ -5,59 +5,25 @@ import static java.util.Objects.requireNonNull;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import java.net.URI;
-import java.util.Objects;
 import no.unit.nva.model.instancetypes.PublicationInstance;
 import no.unit.nva.model.pages.NullPages;
-import nva.commons.core.JacocoGenerated;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-public class SoftwareSourceCode implements PublicationInstance<NullPages> {
+public record SoftwareSourceCode(
+    @JsonProperty(value = SoftwareSourceCode.SOFTWARE_VERSION_FIELD, required = true)
+        String softwareVersion,
+    @JsonProperty(SoftwareSourceCode.CODE_REPOSITORY_FIELD) URI codeRepository)
+    implements PublicationInstance<NullPages> {
 
   public static final String SOFTWARE_VERSION_FIELD = "softwareVersion";
   public static final String CODE_REPOSITORY_FIELD = "codeRepository";
 
-  @JsonProperty(SOFTWARE_VERSION_FIELD)
-  private final String softwareVersion;
-
-  @JsonProperty(CODE_REPOSITORY_FIELD)
-  private final URI codeRepository;
-
-  public SoftwareSourceCode(
-      @JsonProperty(SOFTWARE_VERSION_FIELD) String softwareVersion,
-      @JsonProperty(CODE_REPOSITORY_FIELD) URI codeRepository) {
-    this.softwareVersion = requireNonNull(softwareVersion, "softwareVersion is required");
-    this.codeRepository = codeRepository;
-  }
-
-  public String getSoftwareVersion() {
-    return softwareVersion;
-  }
-
-  public URI getCodeRepository() {
-    return codeRepository;
+  public SoftwareSourceCode {
+    requireNonNull(softwareVersion, "softwareVersion is required");
   }
 
   @Override
   public NullPages getPages() {
     return NullPages.NULL_PAGES;
-  }
-
-  @JacocoGenerated
-  @Override
-  public boolean equals(Object other) {
-    if (this == other) {
-      return true;
-    }
-    if (!(other instanceof SoftwareSourceCode software)) {
-      return false;
-    }
-    return Objects.equals(getSoftwareVersion(), software.getSoftwareVersion())
-        && Objects.equals(getCodeRepository(), software.getCodeRepository());
-  }
-
-  @JacocoGenerated
-  @Override
-  public int hashCode() {
-    return Objects.hash(getSoftwareVersion(), getCodeRepository());
   }
 }
