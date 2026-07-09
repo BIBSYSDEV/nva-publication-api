53b27142dc1992eeb959aaf567ef45700c941a54
brinxmat <789709+brinxmat@users.noreply.github.com>
2026-07-09 12:09:34 +0200

    Use constant

--- publication-file-text/src/main/java/no/unit/nva/publication/file/text/LogSanitizer.java
+++ publication-file-text/src/main/java/no/unit/nva/publication/file/text/LogSanitizer.java
@@ -8,6 +8,7 @@ final class LogSanitizer {
   private static final int MAX_LENGTH = 256;
   private static final String LOG_INJECTION_CHARS = "[\\r\\n\\t]";
   private static final String LOG_INJECTION_REPLACEMENT = "_";
+  private static final String EMPTY_STRING = "";
 
   private LogSanitizer() {
     // NO-OP
@@ -15,7 +16,7 @@ final class LogSanitizer {
 
   static String sanitize(String value) {
     if (isNull(value)) {
-      return "";
+      return EMPTY_STRING;
     }
     var truncated = value.length() > MAX_LENGTH ? value.substring(0, MAX_LENGTH) : value;
     return truncated.replaceAll(LOG_INJECTION_CHARS, LOG_INJECTION_REPLACEMENT);
