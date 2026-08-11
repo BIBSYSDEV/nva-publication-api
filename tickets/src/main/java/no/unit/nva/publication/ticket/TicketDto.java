6271f87213735f2a7a8c65ee24e7bd02a7714ab9
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-08-11 08:01:28 +0200

    build: Upgrade dependencies

--- tickets/src/main/java/no/unit/nva/publication/ticket/TicketDto.java
+++ tickets/src/main/java/no/unit/nva/publication/ticket/TicketDto.java
@@ -35,7 +35,7 @@ import no.unit.nva.publication.permissions.ticket.TicketPermissions;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UriWrapper;
 
-@SuppressWarnings({"PMD.ExcessiveParameterList", "PMD.CouplingBetweenObjects"})
+@SuppressWarnings("PMD.ExcessiveParameterList")
 @JsonTypeInfo(use = Id.NAME, property = "type")
 @JsonSubTypes({
   @JsonSubTypes.Type(DoiRequestDto.class),
